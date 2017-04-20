98448d10a141d2113c82bdc5ab102a3531dae811
Yanyan Jiang <jiangyy@outlook.com>
2017-04-20 14:41:59 -0400

    add a benchmark framework

---
+++ apps/microbench/src/qsort/qsort.c
@@ -0,0 +1,7 @@
+#include <benchmark.h>
+
+void bench_qsort_prepare() {
+}
+
+void bench_qsort_run() {
+}
---
+++ apps/microbench/src/rbtree/rbtree.c
@@ -0,0 +1,8 @@
+#include <benchmark.h>
+
+void bench_rbtree_prepare() {
+}
+
+void bench_rbtree_run() {
+}
+
