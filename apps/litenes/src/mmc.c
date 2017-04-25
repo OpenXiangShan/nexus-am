5c2da2ea3ae0a0618703a156d613abbd3edca837
Yanyan Jiang <jiangyy@outlook.com>
2017-04-24 20:47:27 -0400

    make litenes smaller

--- apps/litenes/src/mmc.c
+++ apps/litenes/src/mmc.c
@@ -2,13 +2,12 @@
 #include <mmc.h>
 #include <ppu.h>
 
-#define MMC_MAX_PAGE_COUNT 4
+#define MMC_MAX_PAGE_COUNT 1
 
 byte mmc_id;
 
-byte mmc_prg_pages[MMC_MAX_PAGE_COUNT][0x4000];
 byte mmc_chr_pages[MMC_MAX_PAGE_COUNT][0x2000];
-int mmc_prg_pages_number, mmc_chr_pages_number;
+int mmc_chr_pages_number;
 
 byte memory[0x10000];
 
@@ -19,12 +18,6 @@ inline byte mmc_read(word address)
 
 inline void mmc_write(word address, byte data)
 {
-    switch (mmc_id) {
-        case 0x3: {
-            ppu_copy(0x0000, &mmc_chr_pages[data & 3][0], 0x2000);
-        }
-        break;
-    }
     memory[address] = data;
 }
 
