458c078ab62f56c6a96fc17410067db824593371
reccetear <16102666004@qq.com>
2017-04-26 03:12:58 +0800

    add time intr

--- am/arch/mips32-npc/src/asym.c
+++ am/arch/mips32-npc/src/asym.c
@@ -2,9 +2,33 @@
 #include <npc.h>
 #include <arch.h>
 
+ulong npc_cycles = 0;
+ulong npc_time = 1;
+
+u32 GetCount(){
+  u32 tick = 0;
+  asm volatile("mfc0 %0, $9\n\t":"=r"(tick));
+  return tick;
+}
+
+void SetCompare(u32 compare){
+  asm volatile("mtc0 %0, $11\n\t"::"r"(compare));
+}
+
+ulong _uptime() {
+  return npc_time;
+}
+
+void _time_event(){
+  npc_time++;
+}
+
+ulong _cycles(){
+  return 0;
+}
+
 void _asye_init(){
   u32 count= GetCount();
-  _putc('g');
   SetCompare(count + 20000);
 }
 
@@ -40,11 +64,16 @@ void irq_handle(struct TrapFrame *tf){
   u32 intr = 0;
   asm volatile("add %0,$k0,$zero\n\t":"=r"(intr));
   switch(intr){
-    case 0x80://time interrupt
+    case 0x7://time interrupt
     {
+      _time_event();
       u32 count= GetCount();
-      _putc('g');
-      SetCompare(count + 20000);
+      if(count + 20000 > 2147483647){
+        SetCompare(count + 20000 - 2147483647);
+      }
+      else{
+        SetCompare(count + 20000);
+      }
     }break;
     default:_putc('0' + intr);
   }
--- am/arch/mips32-npc/src/trm.c
+++ am/arch/mips32-npc/src/trm.c
@@ -3,8 +3,6 @@
 
 _Area _heap;
 _Screen _screen;
-ulong npc_cycles = 0;
-ulong npc_time = 0;
 
 void _trm_init() {
   serial_init();
@@ -23,24 +21,6 @@ void _halt(int code) {
   _putc('c');
   _putc('\n');
   while(1);
-}
-
-u32 GetCount(){
-  u32 tick = 0;
-  asm volatile("mfc0 %0, $9\n\t":"=r"(tick));
-  return tick;
-}
-
-void SetCompare(u32 compare){
-  asm volatile("mtc0 %0, $11\n\t"::"r"(compare));
-}
-
-ulong _uptime() {
-  return npc_time ++;
-}
-
-ulong _cycles(){
-  return 0;
 }
 
 void memory_init(){
