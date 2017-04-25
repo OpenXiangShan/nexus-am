5b02fe6f38455899f902fc5b3e3d9e75e424cd7c
reccetear <16102666004@qq.com>
2017-04-26 00:04:36 +0800

    clean up Ccode

--- am/arch/mips32-npc/src/trm.c
+++ am/arch/mips32-npc/src/trm.c
@@ -1,16 +1,44 @@
 #include <am.h>
 #include <npc.h>
 
+_Area _heap;
+_Screen _screen;
+ulong npc_cycles = 0;
+ulong npc_time = 0;
+
+void _trm_init() {
+  serial_init();
+  memory_init();
+}
+
+void _ioe_init() {
+  vga_init();
+}
+
+void _halt(int code) {
+  _putc('P');
+  _putc('a');
+  _putc('n');
+  _putc('i');
+  _putc('c');
+  _putc('\n');
+  while(1);
+}
+
+ulong _uptime() {
+  return npc_time ++;
+}
+
+ulong _cycles(){
+  return npc_cycles ++;
+}
+
 void memory_init(){
-//0-7fff for code(32k)
-//8000-ffff for data(32k)
-//10000-17fff for stack(32k)
-//18000-1ffff for heap(32k)
-	//probe a memory for heap
-	_heap.start = (void *)HEAP_START;
-	_heap.end = (void *)HEAP_END;
+  //probe a memory for heap
+  _heap.start = (void *)HEAP_START;
+  _heap.end = (void *)HEAP_END;
 }
 
 void serial_init(){
-	//not to do
+  //not to do
 }
