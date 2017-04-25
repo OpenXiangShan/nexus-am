afcf062044151518ad5f8031c42ed9d8f344d2b3
reccetear <16102666004@qq.com>
2017-04-26 01:16:37 +0800

    fix Makefile bug dont need bal

--- am/arch/mips32-npc/src/asym.c
+++ am/arch/mips32-npc/src/asym.c
@@ -2,12 +2,18 @@
 #include <npc.h>
 #include <arch.h>
 
+void _asye_init(){
+  u32 count= GetCount();
+  _putc('g');
+  SetCompare(count + 20000);
+}
+
 void _listen(_RegSet* (*l)(int ex, _RegSet *regs)){
 }
 
 _RegSet *_make(_Area kstack, void *entry){
-	_RegSet *r = (void *)0;
-	return r;
+  _RegSet *r = (void *)0;
+  return r;
 }
 
 void _trap(){
@@ -28,19 +34,19 @@ int _istatus(){
 
 void irq_handle(struct TrapFrame *tf){
 //TODO:handle
-	u32 arg = 0;
-	asm volatile("add %0,$k1,$zero\n\t":"=r"(arg));
-	tf = (void *)arg;
-	u32 intr = 0;
-	asm volatile("add %0,$k0,$zero\n\t":"=r"(intr));
-	switch(intr){
-		case 0x80://time interrupt
-			{
-				u32 count= GetCount();
-				_putc('g');
-				SetCompare(count + 20000);
-			}break;
-		default:_putc('0' + intr);
-	}
-	asm volatile("nop");
+  u32 arg = 0;
+  asm volatile("add %0,$k1,$zero\n\t":"=r"(arg));
+  tf = (void *)arg;
+  u32 intr = 0;
+  asm volatile("add %0,$k0,$zero\n\t":"=r"(intr));
+  switch(intr){
+    case 0x80://time interrupt
+    {
+      u32 count= GetCount();
+      _putc('g');
+      SetCompare(count + 20000);
+    }break;
+    default:_putc('0' + intr);
+  }
+  asm volatile("nop");
 }
--- am/arch/mips32-npc/src/trm.c
+++ am/arch/mips32-npc/src/trm.c
@@ -25,12 +25,22 @@ void _halt(int code) {
   while(1);
 }
 
+u32 GetCount(){
+  u32 tick = 0;
+  asm volatile("mfc0 %0, $9\n\t":"=r"(tick));
+  return tick;
+}
+
+void SetCompare(u32 compare){
+  asm volatile("mtc0 %0, $11\n\t"::"r"(compare));
+}
+
 ulong _uptime() {
   return npc_time ++;
 }
 
 ulong _cycles(){
-  return npc_cycles ++;
+  return 0;
 }
 
 void memory_init(){
