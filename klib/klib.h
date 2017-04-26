9de6c1d85a647c60840ba809452acfd1cba79502
rtt <141220098@smail.nju.edu.cn>
2017-04-26 03:44:21 -0700

    string.c updated TODO: atoi itoa print.c

--- klib/klib.h
+++ klib/klib.h
@@ -12,11 +12,16 @@ extern "C" {
 #endif
 
 // We're expecting:
-//   assert, printf, sprintf
+//   assert, printk, sprintk
 //   memcpy, memset, strcpy, strlen, itoa, atoi, ...
 //   kalloc, kree
 
 size_t strlen(const char *s);
+char* strcpy(char *dst,const char *src);
+void* memset(void* v,int c,size_t n);
+void* memcpy(void* dst,const void*src,size_t n);
+int printk(const char *format, ...);
+int sprintk(char *out, const char *format, ...);
 int rand();
 
 #ifdef __cplusplus
---
+++ klib/printk.c
@@ -0,0 +1,17 @@
+#include<klib.h>
+
+void printchar(char** str,int c){
+//	if(str){
+//		**str=c;
+//		++(*str);
+//	}
+//	else putchar(c);
+	return;
+}
+
+int printk(const char*format,...){
+	//va_list args;
+	//va_starrt(args,format);
+	//return print(0,format,args);
+	return 0;
+}
