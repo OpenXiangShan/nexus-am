d0948930b6bf8bd5b4fa9d7b05697b9eb69b3a17
Yanyan Jiang <jiangyy@outlook.com>
2017-04-27 22:28:11 -0400

    remove hack code

--- klib/printk.c
+++ klib/printk.c
@@ -1,12 +1,6 @@
 #include<am.h>
 #include<klib.h>
-
-typedef char* va_list;
-
-#define _INTSIZEOF(n)   ( (sizeof(n) + sizeof(int) - 1) & ~(sizeof(int) - 1) )
-#define va_start(ap,v)  ( ap = (va_list)&v + _INTSIZEOF(v) )
-#define va_arg(ap,t)    ( *(t *)((ap += _INTSIZEOF(t)) - _INTSIZEOF(t)) )
-#define va_end(ap)      ( ap = (va_list)0 )
+#include<stdarg.h>
 
 void printch(char ch);
 void printdec(int dec);
