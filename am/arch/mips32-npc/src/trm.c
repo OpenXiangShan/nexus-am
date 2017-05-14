baad1ead4cf5f7123ae9afca3094b712bdcac701
reccetear <16102666004@qq.com>
2017-05-14 23:45:13 +0800

    change loader

--- am/arch/mips32-npc/src/trm.c
+++ am/arch/mips32-npc/src/trm.c
@@ -6,9 +6,9 @@
 _Area _heap;
 _Screen _screen;
 
-extern unsigned int _heap_start;
-extern unsigned int __bss_start;
-extern unsigned int __bss_end;
+extern char _heap_start;
+extern char __bss_start;
+extern char __bss_end;
 
 void _trm_init() {
   serial_init();
@@ -31,16 +31,12 @@ void _halt(int code) {
 
 void memory_init(){
   //probe a memory for heap
-  volatile unsigned int st = _heap_start;
-  volatile unsigned int ed = _heap_start + MAX_MEMORY_SIZE;
-  _heap.start = (void *)st;
-  _heap.end = (void *)ed;
-  st = __bss_start;
-  ed = __bss_end;
-  volatile char *bss = (void *)st;
-  volatile unsigned int i;
-  for(i = st; i < ed; i++) {
-    bss[i - st] = 0 ;
+  _heap.start = (void *)(&_heap_start);
+  _heap.end = (char *)(&_heap_start) + MAX_MEMORY_SIZE;
+  volatile char *st = (void *)(&__bss_start);
+  volatile char *ed = (void *)(&__bss_end);
+  for(; st != ed; st++) {
+    *st = 0 ;
   }
 }
 
