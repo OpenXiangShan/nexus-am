5e349a14c6da7330e2bf4f6530d17088e09e8cfc
rtt <141220098@smail.nju.edu.cn>
2017-05-19 02:29:49 -0700

    klibtest测试代码更新

--- apps/klibtest/src/hello.c
+++ apps/klibtest/src/hello.c
@@ -6,15 +6,40 @@
 int main() {
   _trm_init();
   _ioe_init();
-  char src[10]="hello";
+
+
+  printk("printf test begin...\n");
+  printk("the answer should be:\n");
+  printk("#######################################################\n");
+  printk("Hello, welcome to OSlab! I'm the body of the game. \n");
+  printk("Bootblock loads me to the memory position of 0x100000, and Makefile also tells me that I'm at the location of 0x100000. \n");
+  printk("~!@#\(^&*()_+`1234567890-=...... \n");
+  printk("Now I will test your printf: \n");
+  printk("1 + 1 = 2, 123 * 456 = 56088\n0, -1, -2147483648, -1412505855, -32768, 102030\n0, ffffffff, 80000000, abcdef01, ffff8000, 18e8e\n");
+  printk("#######################################################\n");
+  printk("your answer:\n");
+  printk("=======================================================\n");
+  printk("%s %s%scome %co%s", "Hello,", "", "wel", 't', " ");
+  printk("%c%c%c%c%c! ", 'O', 'S', 'l', 'a', 'b');
+  printk("I'm the %s of %s. %s 0x%x, %s 0x%x. \n", "body", "the game", "Bootblock loads me to the memory position of", 0x100000, "and Makefile also tells me that I'm at the location of", 0x100000);
+  printk("~!@#\(^&*()_+`1234567890-=...... ");
+  printk("Now I will test your printf: ");
+  printk("%d + %d = %d, %d * %d = %d\n", 1, 1, 1 + 1, 123, 456, 123 * 456);
+  printk("%d, %d, %d, %d, %d, %d\n", 0, 0xffffffff, 0x80000000, 0xabcedf01, -32768, 102030);
+  printk("%x, %x, %x, %x, %x, %x\n", 0, 0xffffffff, 0x80000000, 0xabcedf01, -32768, 102030);
+  printk("=======================================================\n");
+  printk("Test end!!! Good luck!!!\n");
+
+
+  //char src[10]="hello";
   //char dst[10];
   //strcpy(dst,src);
   //strncpy(dst,src,5);
-  memcpy(src+1,src,5);
+  //memcpy(src+1,src,5);
   //strcat(dst,src);
   //sprintf(dst,"%d%s%c",1024,src,'t');
   //printk("%d%s",memcmp(src,dst,6),src);
-  printk("%3d",1);
+  //printk("%-8d\n%03d\n%c\n%s\n",15,1,'c',src,src);
   assert(1==0);
   //for (int i = 0; i < 10; i ++) {
   //  print("Hello World!\n");
--- klib/printk.c
+++ klib/printk.c
@@ -1,34 +1,67 @@
-/*
+#include<am.h>
+#include<klib.h>
+#include<stdarg.h>
+
 char* printch(char ch,char* s);
-char* printdec(int dec,char* s);
-char* vprintdec(int dec,char* s);
+char* printdec(int dec,int base,int width,char flagc,char* s);
+int vprintdec(int dec,int base,int width,char flagc,char* s);
+//char* printstr(char* str,int width,char flagc,char* s);
 char* printstr(char* str,char* s);
 
-//int vprintk(
-
-int printk(const char *fmt, ...){
-	//return vprintk(0,fmt,...);
+int vprintk(char* out,const char* fmt,va_list ap){
 	int vargint=0;
 	char* vargpch=0;
 	char vargch=0;
-	
-	const char* pfmt=0;
-	va_list vp;
-
-	va_start(vp,fmt);
-	pfmt=fmt;
-
+	char flagc=' ';
+	int base,width=-1;
+	const char* pfmt=fmt;
 	while(*pfmt){
 		if(*pfmt == '%'){
+			flagc=' ';
+			width=-1;
+		reswitch:
+			//flagc=' ';
+			//width=-1;
 			switch(*(++pfmt)){
-				case 'c':vargch=va_arg(vp,int);printch(vargch,0);break;
-				case 'd':
-				case 'i':vargint=va_arg(vp,int);vprintdec(vargint,0);break;
-				case 's':vargpch=va_arg(vp,char*);printstr(vargpch,0);break;
-				case 'u':
+				case '-':flagc='-';goto reswitch;
+				case '0':flagc='0';goto reswitch;
+				case ' ':flagc=' ';goto reswitch;
+				case '+':flagc='+';goto reswitch;
+				case '1':
+				case '2':
+				case '3':
+				case '4':
+				case '5':
+				case '6':
+				case '7':
+				case '8':
+				case '9':
+					width=0;
+					char ch;
+					while( (ch=*pfmt++)>='0'&&ch<='9'){
+						width=width*10+ch-'0';
+						//_putc(width+'0');
+					}
+					pfmt-=2;
+					//_putc(*(pfmt));
+					goto reswitch;
+				case '*':width=va_arg(ap,int);goto reswitch;
+				//case '.':if(width<0)width=0;goto reswitch;
+				case 'c':vargch=va_arg(ap,int);printch(vargch,0);break;
+				case 'd':vargint=va_arg(ap,int);base=10;if(vargint<0){
+						 _putc('-');
+						 vargint=-vargint;
+					 }
+					 else if(flagc=='+'){flagc=' ';_putc('+');}
+					 goto nump;
+				//case 'o':vargint=va_arg(ap,unsigned int);base=8;goto nump;
+				case 'u':vargint=va_arg(ap,unsigned int);base=10;goto nump;
 				case 'x':
-				case 'X':
-				case 'p':
+				case 'X':vargint=va_arg(ap,int);base=16;goto nump;
+				case 'p':_putc('0');_putc('x');vargint=(long)va_arg(ap,void*);base=16;goto nump;
+			nump:
+					 printdec(vargint,base,width,flagc,0);break;
+				case 's':vargpch=va_arg(ap,char*);printstr(vargpch,0);break;
 				default:;
 			}
 			pfmt++;
@@ -37,38 +70,91 @@ int printk(const char *fmt, ...){
 			printch(*pfmt++,0);
 		}
 	}
-	va_end(vp);
 	return 0;
 }
+
+int printk(const char* fmt,...){
+	va_list ap;
+	va_start(ap,fmt);
+	int r=vprintk(0,fmt,ap);
+	va_end(ap);
+	return r;
+}
+
 int sprintf(char* out,char* fmt,...){
-	int vargint=0;
-	char* vargpch=0;
-	char vargch=0;
-	
-	char* pfmt=0;
-	va_list vp;
-
-	va_start(vp,fmt);
-	pfmt=fmt;
-
-	while(*pfmt){
-		if(*pfmt == '%'){
-			switch(*(++pfmt)){
-				case 'c':vargch=va_arg(vp,int);out=printch(vargch,out);break;
-				case 'd':
-				case 'i':vargint=va_arg(vp,int);out=vprintdec(vargint,out);break;
-				case 's':vargpch=va_arg(vp,char*);out=printstr(vargpch,out);break;
-				default:;
-			}
-			pfmt++;
-		}
-		else{
-			out=printch(*pfmt++,out);
-		}
-	}
-	va_end(vp);
-	*out='\0';
-	return 0;
-
+	va_list ap;
+	va_start(ap,fmt);
+	int r=vprintk(out,fmt,ap);
+	va_end(ap);
+	return r;
 }
-*/
+char* printch(char ch,char* s){
+	if(s==0)_putc(ch);
+	else *s++=ch;
+	return s;
+}
+//char* printdec(int dec,char* s){
+char* printdec(int dec,int base,int width,char flagc,char* s){
+	if(dec==0){
+		_putc('0');
+	}
+	else vprintdec(dec,base,width,flagc,s);
+	return s;
+}
+int vprintdec(int dec,int base,int width,char flagc,char* s){
+	/*if(dec<0){
+		_putc('-');
+		vprintdec(-dec,s);
+		return s;
+	}*/
+	//_putc(flagc);
+	if(dec==0){
+		if(flagc!='-'){
+			//_putc(width+'0');
+			while(width-->0){
+				if(s==0)_putc(flagc);
+				else *s++=flagc;
+			}
+			return 0;
+		}
+		return width;
+	}
+	//_putc(width+'0');
+	//assert(base==16);
+	//_putc((char)(dec%base+'0'));
+	int re;//vprintdec(dec/base,base,width-1,flagc,s+1);
+	if(s==0){
+		re=vprintdec(dec/base,base,width-1,flagc,s);
+		if(flagc=='-'){
+			while(re-->0)_putc(' ');
+		}
+		if(dec%base>9)_putc(dec%base+'a'-10);
+		else _putc((char)(dec%base+'0'));
+	}
+	else {
+		re=vprintdec(dec/base,base,width-1,flagc,s+1);
+		if(flagc=='-'){
+			while(re-->0)*s++=' ';
+		}
+		if(dec%base>9)*s=(char)(dec%base+'a'-10);
+		else *s=(char)(dec%base+'0');
+	}
+	/*
+	if(flagc=='-'){
+		while(re-->0){
+			if(s==0)_putc(' ');
+			else *s++=' ';
+		}
+	}*/
+	return re;
+}
+
+char* printstr(char* str,char* s){
+	while(*str){
+		//_putc(*str);
+		if(s==0)_putc(*str++);
+		else*s++=*str++;
+	}
+	//*s++='\0';
+	return s;
+}
