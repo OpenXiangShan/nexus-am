2b61b3afa000b086ba077157836a6fbc565eea53
Yanyan Jiang <jiangyy@outlook.com>
2017-05-26 12:46:07 -0400

    refactor game

---
+++ apps/type-game/draw.c
@@ -0,0 +1,70 @@
+#include "game.h"
+
+static _Pixel canvas[W][H];
+
+extern char font8x8_basic[128][8];
+
+char *itoa(int n)  {  
+  static char s[64];
+  int i = sizeof(s) - 1;
+  do {
+    s[--i] = n % 10 + '0';  
+    n /= 10;
+  } while(n > 0);  
+  return &s[i];
+}  
+
+static inline void draw_character(char ch, int x, int y, int color) {
+  int i, j;
+  char *p = font8x8_basic[(int)ch];
+  for (i = 0; i < 8; i ++) 
+    for (j = 0; j < 8; j ++) 
+      if ((p[i] >> j) & 1)
+        if (y + j < W && x + i < H)
+          canvas[y + j][x + i] = color;
+}
+
+static inline void draw_string(const char *str, int x, int y, int color) {
+  while (*str) {
+    draw_character(*str ++, x, y, color);
+    if (y + 8 >= W) {
+      x += 8; y = 0;
+    } else {
+      y += 8;
+    }
+  }
+}
+
+void redraw_screen() {
+  fly_t it;
+  const char *hit, *miss;
+
+  /* 绘制每个字符 */
+  for (it = characters(); it != NULL; it = it->_next) {
+    static char buf[2];
+    buf[0] = it->text + 'A'; buf[1] = 0;
+    draw_string(buf, it->x, it->y, 0xffffffff);
+  }
+
+  /* 绘制命中数、miss数、最后一次按键扫描码和fps */
+  const char *key = itoa(last_key_code());
+  draw_string(key, W - 8, 0, 0xffffffff);
+  hit = itoa(get_hit());
+  draw_string(hit, 0, W - strlen(hit) * 8, 0x00ff00);
+  miss = itoa(get_miss());
+  draw_string(miss, H - 8, W - strlen(miss) * 8, 0xfa5858);
+  const char *fps = itoa(get_fps());
+  draw_string(fps, 0, 0, 0xf3f781);
+  draw_string("FPS", 0, strlen(fps) * 8, 0xf3f781);
+
+  int w = _screen.width, h = _screen.height;
+  for (int y = 0; y < w; y ++)
+    for (int x = 0; x < h; x ++) {
+      _draw_p(y, x, canvas[y * W / w][x * H / h]);
+    }
+
+  _draw_sync();
+  for (int y = 0; y < W; y ++)
+    for (int x = 0; x < H; x ++) 
+      canvas[y][x] = 0x2a0a29;
+}
--- apps/type-game/game.h
+++ apps/type-game/game.h
@@ -1,5 +1,17 @@
-#ifndef __LINKLIST_H__
-#define __LINKLIST_H__
+#ifndef common_h
+#define common_h
+
+#define HZ 100
+#define FPS 30
+#define CHARACTER_PER_SECOND 10
+#define UPDATE_PER_SECOND 100
+#define NULL ((void *)0)
+
+#define W 320
+#define H 200
+
+#include <am.h>
+#include <klib.h>
 
 /* Usage:
  *   my_list.h:
@@ -77,5 +89,38 @@ void NAME##_free(NAME##_t node) { \
 	node->_next = NAME##_free_head; \
 	NAME##_free_head = node; \
 }
+
+/* 定义fly_t链表 */
+LINKLIST_DEF(fly)
+	int x;
+	int y;
+	int text;
+	int v;
+LINKLIST_DEF_FI(fly)
+
+typedef char bool;
+
+#define true         1
+#define false        0
+
+/* 按键相关 */
+void press_key(int scan_code);
+void release_key(int ch);
+bool query_key(int ch);
+int last_key_code(void);
+bool keyboard_event();
+
+/* 游戏逻辑相关 */
+void create_new_letter(void);
+void update_letter_pos(void);
+bool update_keypress(void);
+
+int get_hit(void);
+int get_miss(void);
+int get_fps(void);
+void set_fps(int fps);
+fly_t characters(void);
+
+void redraw_screen();
 
 #endif
