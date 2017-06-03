7eb011369597e79ce88c6b0d96e7c732ff5dc87a
Yanyan Jiang <jiangyy@outlook.com>
2017-06-03 07:45:08 -0400

    add ioe init and start message

--- apps/coremark/src/core_main.c
+++ apps/coremark/src/core_main.c
@@ -100,6 +100,11 @@ MAIN_RETURN_TYPE main(int argc, char *argv[]) {
 #if (MEM_METHOD==MEM_STACK)
 	ee_u8 stack_memblock[TOTAL_DATA_SIZE*MULTITHREAD];
 #endif
+
+  _ioe_init();
+
+  ee_printf("Running CoreMark for %d iterations\n", ITERATIONS);
+
 	/* first call any initializations needed */
 	portable_init(&(results[0].port), &argc, argv);
 	/* First some checks to make sure benchmark will run ok */
