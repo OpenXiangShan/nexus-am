d2bb8915a5612466427a80867029cfd22b425e37
yzh <yuzihao@ict.ac.cn>
2017-06-06 18:11:12 +0800

    am,arch: add x86-nemu

---
+++ am/arch/x86-nemu/src/trm.c
@@ -0,0 +1,50 @@
+#include <am.h>
+#include <x86-nemu.h>
+
+extern char _heap_start;
+extern char _heap_end;
+extern int main();
+
+_Area _heap = {
+  .start = &_heap_start,
+  .end = &_heap_end,
+};
+
+_Screen _screen = {
+  .width  = 0,
+  .height = 0,
+};
+
+static void serial_init() {
+#ifdef HAS_DEVICE
+  SERIAL_PORT[1] = 0x00;
+  SERIAL_PORT[3] = 0x80;
+  SERIAL_PORT[0] = 0x01;
+  SERIAL_PORT[1] = 0x00;
+  SERIAL_PORT[3] = 0x03;
+  SERIAL_PORT[2] = 0xc7;
+  SERIAL_PORT[4] = 0x0b;
+#endif
+}
+
+void _putc(char ch) {
+#ifdef HAS_DEVICE
+  while ((SERIAL_PORT[5] & 0x20) == 0);
+  SERIAL_PORT[0] = ch;
+#else
+  asm volatile(".byte 0xd6" : :"a"(2), "b"((u8)ch));
+#endif
+}
+
+void _halt(int code) {
+  asm volatile(".byte 0xd6" : :"a"(code));
+
+  // should not reach here
+  while (1);
+}
+
+void _trm_init() {
+  serial_init();
+  int ret = main();
+  _halt(ret);
+}
