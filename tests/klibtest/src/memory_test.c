d8df2f561e7b24dc4019c48c63ba29d70e082eb6
rtt <141220098@smail.nju.edu.cn>
2017-06-13 10:57:24 -0700

    strcpy测试完成

--- tests/klibtest/src/memory_test.c
+++ tests/klibtest/src/memory_test.c
@@ -1,11 +1,11 @@
 #include <klib.h>
 
-#define N 64
+#define N 8
 static u8 data[N];
 
 static void reset() {
   for (int i = 0; i < N; i ++) {
-    data[i] = i;
+    data[i] = i+1;
   }
 }
 
@@ -17,7 +17,8 @@ static void check_equal(int st, int ed, int val) {
 
 static void check_sequence(int st, int ed, int initval) {
   for (int i = st; i < ed; i ++) {
-    assert(data[i] == initval + i - st);
+    //printk("data[%d]=%d,should be %d\n",i,data[i],initval + i - st + 1);
+    assert(data[i] == initval + i - st + 1);
   }
 }
 
@@ -63,24 +64,23 @@ void memory_test() {
       }
     }
   }
-  /*
   for (int st = 0; st < N; st ++) {
     for (int ed = st + 1; ed <= N; ed ++) {
       int len = ed - st;
       for (int cp = 0; cp + len <= N; cp ++) {
 	//这里是因为 strcpy的内部是不会处理内存重叠问题，标准库中的strcpy也会出错，所以测试就不测试内存重叠了。
         reset();
-        if((cp > st && cp < ed)||(cp + len > st && cp + len < ed)) continue;
-	data[cp + len]='\0';
+        if((cp >= st && cp < ed)||(cp + len > st && cp + len <= ed)) continue;
+	data[cp + len-1]='\0';
 	strcpy((char *)data + st, (char *)data + cp);
-	data[cp + len]=cp + len;
-        //check_sequence(0, st, 0);
-        //check_sequence(st, st + len - 1, cp);
-        //check_sequence(ed, N, st + len);
-	//printk("%d",cp);
+	data[cp + len-1]=cp + len;
+        check_sequence(0, st, 0);
+        check_sequence(st, st + len - 1, cp);
+	assert(data[ed-1]=='\0');
+        check_sequence(ed, N, st + len);
       }
     }
-  }*/
+  }
 
   // strcmp / strncmp: TODO
 }
