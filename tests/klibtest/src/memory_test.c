40e615dd5be9e70ebeb200495cec08f68e97698c
rtt <141220098@smail.nju.edu.cn>
2017-06-12 23:44:42 -0700

    strncpy测试

--- tests/klibtest/src/memory_test.c
+++ tests/klibtest/src/memory_test.c
@@ -33,7 +33,7 @@ void memory_test() {
       check_equal(st, ed, val);
     }
 
-  // memmove
+  // memmove memcpy
   for (int st = 0; st < N; st ++) {
     for (int ed = st + 1; ed <= N; ed ++) {
       int len = ed - st;
@@ -47,9 +47,40 @@ void memory_test() {
     }
   }
 
-  // memcpy: TODO
+  // memcpy: memcpy是用的memmove实现所以共用一个测试即可
 
   // strcpy / strncpy: TODO
+  for (int st = 0; st < N; st ++) {
+    for (int ed = st + 1; ed <= N; ed ++) {
+      int len = ed - st;
+      for (int cp = 0; cp + len <= N; cp ++) {
+        reset();
+        if((cp > st && cp < ed)||(cp + len > st && cp + len < ed)) continue;
+	strncpy((char *)data + st,(char *)data + cp,len);
+        check_sequence(0, st, 0);
+        check_sequence(st, st + len, cp);
+        check_sequence(st + len, N, st + len);
+      }
+    }
+  }
+  /*
+  for (int st = 0; st < N; st ++) {
+    for (int ed = st + 1; ed <= N; ed ++) {
+      int len = ed - st;
+      for (int cp = 0; cp + len <= N; cp ++) {
+	//这里是因为 strcpy的内部是不会处理内存重叠问题，标准库中的strcpy也会出错，所以测试就不测试内存重叠了。
+        reset();
+        if((cp > st && cp < ed)||(cp + len > st && cp + len < ed)) continue;
+	data[cp + len]='\0';
+	strcpy((char *)data + st, (char *)data + cp);
+	data[cp + len]=cp + len;
+        //check_sequence(0, st, 0);
+        //check_sequence(st, st + len - 1, cp);
+        //check_sequence(ed, N, st + len);
+	//printk("%d",cp);
+      }
+    }
+  }*/
 
   // strcmp / strncmp: TODO
 }
