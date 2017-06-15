4a48a126277673233494d9c6dc4f6f55adcce13a
rtt <141220098@smail.nju.edu.cn>
2017-06-15 06:31:26 -0700

    printk test 完成基本类型

--- tests/klibtest/src/printk_test.c
+++ tests/klibtest/src/printk_test.c
@@ -1,5 +1,22 @@
 #include <klib.h>
 
+#define MAX_INT 0x7fffffff
+#define MIN_INT 0x80000000
+#define MAX_UINT 0xffffffff
+
+static char src[1024];
+static char dst[1024];
+
+static void reset_src(){
+  for(int i = 0; i < 1024; i ++)
+    src[i] = 0;
+}
+
+static void check_src(char* s){
+  assert( 0 == strcmp( s, src));
+}
+
+
 void printk_test() {
   // sprintf: TODO
 
@@ -9,14 +26,40 @@ void printk_test() {
 
   // printk: TODO
   
-  static char src[1024]="hello";
-  printk("0, -1, -2147483648, -1412505855, -32768, 102030\n");
-  printk("%d, %d, %d, %d, %d, %d\n", 0, 0xffffffff, 0x80000000, 0xabcedf01, -32768, 102030);
-  sprintf(src,"%d, %d, %d, %d, %d, %d\n", 0, 0xffffffff, 0x80000000, 0xabcedf01, -32768, 102030);
-  printk("%s",src);
-  printk("0, ffffffff, 80000000, abcdef01, ffff8000, 18e8e\n");
-  printk("%x, %x, %x, %x, %x, %x\n", 0, 0xffffffff, 0x80000000, 0xabcedf01, -32768, 102030);
+  //打印所有可显示ascii字符----c
+    printk(" !\"#$%%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\n");
+  for(int i = 0; i + ' ' <= '~'; i ++){
+    printk("%c", i + ' ');
+    sprintf(src + i, "%c", i + ' ');
+    assert(src[i] ==i + ' ');
+  }
+  //%s
+  printk("\n%s\n", src);
+  sprintf(dst,"%s", src);
+  assert(0 == strcmp( dst, src));
 
+  //打印最大最小值0和各种随机值----d u x X p 
+  //d u
+  reset_src();
+  printk("2147483647,-2147483648,0,102030,-1412505855,4294967295,0\n");
+  printk("%d,%d,%d,%d,%d,%u,%u\n", MAX_INT, MIN_INT, 0, 102030, -1412505855, MAX_UINT, 0);
+  sprintf(src,"%d,%d,%d,%d,%d,%u,%u\n", MAX_INT, MIN_INT, 0, 102030, -1412505855, MAX_UINT, 0);
+  check_src("2147483647,-2147483648,0,102030,-1412505855,4294967295,0\n");
+
+  //x
+  reset_src();
+  printk("0, ffffffff, 80000000, abcdef01, ffff8000, 18e8e\n");
+  printk("%x, %x, %x, %x, %x, %x\n", 0, 0xffffffff, 0x80000000, 0xabcdef01, -32768, 102030);
+  sprintf(src,"%x, %x, %x, %x, %x, %x\n", 0, 0xffffffff, 0x80000000, 0xabcdef01, -32768, 102030);
+  check_src("0, ffffffff, 80000000, abcdef01, ffff8000, 18e8e\n");
+  
+  //p
+  reset_src();
+  printk("0x0, 0xffffffff, 0x80000000, 0xabcdef01, 0xffff8000, 0x18e8e\n");
+  printk("%p, %p, %p, %p, %p, %p\n", 0, 0xffffffff, 0x80000000, 0xabcdef01, -32768, 102030);
+  sprintf(src,"%p, %p, %p, %p, %p, %p\n", 0, 0xffffffff, 0x80000000, 0xabcdef01, -32768, 102030);
+  check_src("0x0, 0xffffffff, 0x80000000, 0xabcdef01, 0xffff8000, 0x18e8e\n");
+  /*这里往后是flag的测试
   printk(" -102030\n");
   printk("%*d\n",8,-102030);
   printk("102030  x\n");
@@ -28,4 +71,5 @@ void printk_test() {
   printk("ffffffff\n");
   printk("%x\n",0xffffffff);
   printk("0, -1, -2\n");
+  */
 }
