9c1a7d488857a8e939ee301c8a47c988d9d6e102
yzh <yuzihao@ict.ac.cn>
2017-06-16 12:22:03 +0800

    am,x86-nemu: add IA32 segmentaion

--- am/arch/x86-nemu/include/x86-nemu.h
+++ am/arch/x86-nemu/include/x86-nemu.h
@@ -1,9 +1,15 @@
 #ifndef __X86_NEMU_H__
 #define __X86_NEMU_H__
 
+/* Uncomment these macros to enable corresponding functionality. */
+#define IA32_SEG
+//#define IA32_PAGE
+//#define IA32_INTR
 //#define HAS_DEVICE
+//#define USE_HARDDISK
 
 #ifndef __ASSEMBLER__
+/* The following code will be included if the source file is a "*.c" file. */
 
 #include <arch.h>
 
