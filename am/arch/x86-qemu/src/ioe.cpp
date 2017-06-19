6fc1a4a7bf3f14521e8b28cd7a1cd9426ada1968
Yanyan Jiang <jiangyy@outlook.com>
2017-06-19 21:13:56 +0800

    remove unused files

--- am/arch/x86-qemu/src/ioe.cpp
+++ am/arch/x86-qemu/src/ioe.cpp
@@ -77,16 +77,6 @@ void _draw_f(_Pixel *p) {
 
 void _draw_sync() {
 }
-#define _KEYS(_) \
-  _(ESCAPE), _(F1), _(F2), _(F3), _(F4), _(F5), _(F6), _(F7), _(F8), _(F9), _(F10), _(F11), _(F12), \
-  _(GRAVE), _(1), _(2), _(3), _(4), _(5), _(6), _(7), _(8), _(9), _(0), _(MINUS), _(EQUALS), _(BACKSPACE), \
-  _(TAB), _(Q), _(W), _(E), _(R), _(T), _(Y), _(U), _(I), _(O), _(P), _(LEFTBRACKET), _(RIGHTBRACKET), _(BACKSLASH), \
-  _(CAPSLOCK), _(A), _(S), _(D), _(F), _(G), _(H), _(J), _(K), _(L), _(SEMICOLON), _(APOSTROPHE), _(RETURN), \
-  _(LSHIFT), _(Z), _(X), _(C), _(V), _(B), _(N), _(M), _(COMMA), _(PERIOD), _(SLASH), _(RSHIFT), \
-  _(LCTRL), _(APPLICATION), _(LALT), _(SPACE), _(RALT), _(RCTRL), \
-  _(UP), _(DOWN), _(LEFT), _(RIGHT), _(INSERT), _(DELETE), _(HOME), _(END), _(PAGEUP), _(PAGEDOWN)
-
-#
 
 static inline int keydown(int e) { return (e & 0x8000) != 0; }
 static inline int upevent(int e) { return e; }
