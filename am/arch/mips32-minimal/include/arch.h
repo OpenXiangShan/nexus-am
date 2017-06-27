5ee8e6bf1a8476ea78fb8002983405dbcf326bbf
Yanyan Jiang <jiangyy@outlook.com>
2017-06-27 06:53:58 -0400

    fix mips32-minimal def

--- am/arch/mips32-minimal/include/arch.h
+++ am/arch/mips32-minimal/include/arch.h
@@ -11,8 +11,7 @@ extern "C" {
 #endif
 
 static inline void _ioe_init() { }
-static inline unsigned long _cycles() { return 0; }
-static inline unsigned long _uptime() { return 0; }
+static inline uintptr_t _uptime() { return 0; }
 static inline void _draw_p(int x, int y, uint32_t p) {}
 static inline void _draw_sync() {}
 
