f2fa76089ccd17bad85035dd0be3f6b7e0c56405
Zihao Yu <yuzihao@ict.ac.cn>
2017-06-28 09:57:59 +0800

    apps,nanos-lite: add ramdisk as part of nanos-lite

--- apps/nanos-lite/src/ramdisk.c
+++ apps/nanos-lite/src/ramdisk.c
@@ -1,8 +1,8 @@
 #include "common.h"
 
-#define RAMDISK_START (void *)0x1000
-#define RAMDISK_END (void *)0xa0000
-#define RAMDISK_SIZE ((RAMDISK_END) - (RAMDISK_START))
+extern uint8_t ramdisk_start;
+extern uint8_t ramdisk_end;
+#define RAMDISK_SIZE ((&ramdisk_end) - (&ramdisk_start))
 
 /* The kernel is monolithic, therefore we do not need to
  * translate the address `buf' from the user process to
@@ -11,12 +11,16 @@
 
 /* read `len' bytes starting from `offset' of ramdisk into `buf' */
 void ramdisk_read(uint8_t *buf, uint32_t offset, uint32_t len) {
-  assert(offset + len < RAMDISK_SIZE);
-  memcpy(buf, RAMDISK_START + offset, len);
+  if (len == 0) return;
+
+  assert(offset + len <= RAMDISK_SIZE);
+  memcpy(buf, &ramdisk_start + offset, len);
 }
 
 /* write `len' bytes starting from `buf' into the `offset' of ramdisk */
 void ramdisk_write(uint8_t *buf, uint32_t offset, uint32_t len) {
-  assert(offset + len < RAMDISK_SIZE);
-  memcpy(RAMDISK_START + offset, buf, len);
+  if (len == 0) return;
+
+  assert(offset + len <= RAMDISK_SIZE);
+  memcpy(&ramdisk_start + offset, buf, len);
 }
