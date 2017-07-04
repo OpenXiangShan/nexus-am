359b8b67ee4e11d62a671e3079944411d99e0348
yzh <yuzihao@ict.ac.cn>
2017-07-04 18:30:21 +0800

    apps,nanos-lite: add fs

---
+++ apps/nanos-lite/include/fs.h
@@ -0,0 +1,14 @@
+#ifndef __FS_H__
+#define __FS_H__
+
+#include "common.h"
+
+enum {SEEK_SET, SEEK_CUR, SEEK_END};
+
+int fs_open(const char *, int, int);
+ssize_t fs_read(int, void *, size_t);
+ssize_t fs_write(int, const void *, size_t);
+off_t fs_lseek(int, off_t, int);
+int fs_close(int);
+
+#endif
