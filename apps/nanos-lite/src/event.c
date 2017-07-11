9b9690fdd9d2bc0637504e0929ca996ccbe765c6
Zihao Yu <yuzihao@ict.ac.cn>
2017-07-11 22:03:41 +0800

    apps,nanos-lite,event: add more keys

--- apps/nanos-lite/src/event.c
+++ apps/nanos-lite/src/event.c
@@ -1,16 +1,18 @@
 #include "common.h"
 
+#define NAME(key) \
+  [_KEY_##key] = #key,
+
+const char *names[256] = {
+  [_KEY_NONE] = "NONE",
+  _KEYS(NAME)
+};
+
 size_t read_events(void *buf) {
   uint32_t time_ms = _uptime();
   int key = _read_key();
   char keydown_char = (key & 0x8000 ? 'd' : 'u');
-  char *keyname = "NO_KEY";
   key &= ~0x8000;
-  if (key == _KEY_SPACE) {
-    keyname = "SPACE";
-  }
 
-  sprintf(buf, "t %d\nk%c %s ", time_ms, keydown_char, keyname);
-  int ret = strlen(buf);
-  return ret;
+  return sprintf(buf, "t %d\nk%c %s ", time_ms, keydown_char, names[key]);
 }
