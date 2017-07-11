21309e94bba30296dada6a4d355c74076ba80c83
Zihao Yu <yuzihao@ict.ac.cn>
2017-07-11 17:16:47 +0800

    am,x86-nemu,ioe: use customized AM keyboard layout

--- am/arch/x86-nemu/src/ioe.c
+++ am/arch/x86-nemu/src/ioe.c
@@ -50,31 +50,15 @@ void _draw_sync() {
 // -------------------- keyboard --------------------
 
 #define KEYDOWN_MASK 0x8000
-int scan_code[] = {
-  0,
-  1, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 87, 88,
-  41, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-  15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 43,
-  58, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 28,
-  42, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54,
-  29, 91, 56, 57, 56, 29,
-  72, 80, 75, 77, 0, 0, 0, 0, 0, 0
-};
 
 int _read_key() {
   int status = inb(0x64);
   if ((status & 0x1) == 0) return _KEY_NONE;
 
   int code = inb(0x60) & 0xff;
-  int keydown_mask = (~code & 0x80) << 8;
+  int keydown_mask = code & 0x80;
   code &= 0x7f;
+  keydown_mask = (keydown_mask && (code != _KEY_NONE) ? KEYDOWN_MASK : 0);
 
-  for (unsigned int i = 0; i < sizeof(scan_code) / sizeof(scan_code[0]); i ++) {
-    if (scan_code[i] == 0) continue;
-    if (scan_code[i] == code) {
-      return i | keydown_mask;
-    }
-  }
-  return _KEY_NONE;
+  return code | keydown_mask;
 }
-
