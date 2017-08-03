3cfde6d30029d446ecbcaddfe890a5cf7de482d7
Zihao Yu <yuzihao@ict.ac.cn>
2017-08-03 23:59:53 +0800

    apps,nanos-lite,loader: use fs api to load the first prog

--- apps/nanos-lite/src/loader.c
+++ apps/nanos-lite/src/loader.c
@@ -2,7 +2,7 @@
 #include "memory.h"
 #include "fs.h"
 
-#define INIT_FILE "/bin/pal"
+#define INIT_FILE "/bin/hello"
 
 void ramdisk_read(void *, off_t, size_t);
 
@@ -11,9 +11,10 @@ void ramdisk_read(void *, off_t, size_t);
 uintptr_t loader() {
   Log("Loading user program %s...", INIT_FILE);
 
-  extern uint8_t ramdisk_start;
-  extern uint8_t ramdisk_end;
-  ramdisk_read((void *)0x4000000, 0, &ramdisk_end - &ramdisk_start);
+  int fd = fs_open(INIT_FILE, 0, 0);
+  ssize_t len = fs_read(fd, (void *)0x4000000, 0x10000000);  // read many enough
+  fs_close(fd);
+  Log("Read %d bytes", len);
 
   return 0x4000000;
 }
