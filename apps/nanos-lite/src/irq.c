775ebf6853ccae709607bcaca5e0465391979c66
Zihao Yu <yuzihao@ict.ac.cn>
2017-08-04 22:50:39 +0800

    apps,nanos-lite,irq,do_event: print a log when receiving a timer interrupt

--- apps/nanos-lite/src/irq.c
+++ apps/nanos-lite/src/irq.c
@@ -6,6 +6,7 @@ _RegSet* do_syscall(uintptr_t *);
 static _RegSet* do_event(_Event e, _RegSet* r) {
   _RegSet *ret = NULL;
   switch (e.event) {
+    case _EVENT_IRQ_TIME: Log("timer intr comes"); break;
     case _EVENT_TRAP: ret = schedule(r); break;
     case _EVENT_SYSCALL: ret = do_syscall((uintptr_t *)e.cause); ret = schedule(r); break;
     default: panic("Unhandled event ID = %d", e.event);
