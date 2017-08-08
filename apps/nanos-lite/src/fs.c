f7bdb6435f03c41eb0f61145f3924551e8b1c344
Zihao Yu <yuzihao@ict.ac.cn>
2017-08-08 17:22:30 +0800

    apps,nanos-lite,fs: simplify fs by using the index of file_table as fd

--- apps/nanos-lite/src/fs.c
+++ apps/nanos-lite/src/fs.c
@@ -4,15 +4,14 @@ typedef struct {
 	char *name;
 	size_t size;
 	off_t disk_offset;
-} file_info;
-
-typedef struct {
-	uint32_t index;
-	off_t offset;
-} Fstate;
+	off_t open_offset;
+} Finfo;
 
 /* This is the information about all files in disk. */
-static file_info file_table[] __attribute__((used)) = {
+static Finfo file_table[] __attribute__((used)) = {
+  {"stdin (note that this is not the actual stdin)", 0, 0},
+  {"stdout (note that this is not the actual stdout)", 0, 0},
+  {"stderr (note that this is not the actual stderr)", 0, 0},
   {"/dev/fb", 0, 0},
   {"/dev/events", 0, 0},
   {"/proc/dispinfo", 128, 0},
@@ -22,21 +21,12 @@ static file_info file_table[] __attribute__((used)) = {
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
 
-Fstate files[FD_NORMAL + NR_FILES];
-
 void init_fs() {
-  int i;
-	for (i = 0; i < NR_FILES; i ++) {
-		if (strcmp(file_table[i].name, "/dev/fb") == 0) {
-      file_table[i].size = _screen.width * _screen.height * sizeof(uint32_t);
-      return;
-		}
-  }
+  file_table[FD_FB].size = _screen.width * _screen.height * sizeof(uint32_t);
 }
 
-
 size_t fs_filesz(int fd) {
-  return file_table[ files[fd].index ].size;
+  return file_table[fd].size;
 }
 
 void ramdisk_read(void *, uint32_t, uint32_t);
@@ -46,18 +36,11 @@ void dispinfo_read(void *buf, off_t offset, size_t len);
 void fb_write(const void *buf, off_t offset, size_t len);
 
 int fs_open(const char *pathname, int flags, int mode) {
-	int i, fd;
+	int i;
 	for (i = 0; i < NR_FILES; i ++) {
 		if (strcmp(file_table[i].name, pathname) == 0) {
-      switch (i) {
-        case 0: fd = FD_FB; break;
-        case 1: fd = FD_EVENTS; break;
-        case 2: fd = FD_DISPINFO; break;
-        default: fd = FD_NORMAL + i; break;
-      }
-			files[fd].index = i;
-			files[fd].offset = 0;
-			return fd;
+			file_table[i].open_offset = 0;
+			return i;
 		}
 	}
 
@@ -71,21 +54,23 @@ ssize_t fs_read(int fd, void *buf, size_t len) {
     return read_events(buf);
   }
 
-	int remain_bytes = file_table[ files[fd].index ].size - files[fd].offset;
+  Finfo *f = file_table + fd;
+	int remain_bytes = f->size - f->open_offset;
 	int bytes_to_read = (remain_bytes > len ? len : remain_bytes);
 
   if (fd == FD_DISPINFO) {
-    dispinfo_read(buf, file_table[files[fd].index].disk_offset + files[fd].offset, bytes_to_read);
+    dispinfo_read(buf, f->disk_offset + f->open_offset, bytes_to_read);
   }
   else {
-    ramdisk_read(buf, file_table[files[fd].index].disk_offset + files[fd].offset, bytes_to_read);
+    ramdisk_read(buf, f->disk_offset + f->open_offset, bytes_to_read);
   }
-	files[fd].offset += bytes_to_read;
+	f->open_offset += bytes_to_read;
 	return bytes_to_read;
 }
 
 ssize_t fs_write(int fd, const void *buf, size_t len) {
-	int remain_bytes = file_table[ files[fd].index ].size - files[fd].offset;
+  Finfo *f = file_table + fd;
+	int remain_bytes = f->size - f->open_offset;
 	int bytes_to_write = (remain_bytes > len ? len : remain_bytes);
   switch (fd) {
     case FD_STDOUT:
@@ -97,22 +82,23 @@ ssize_t fs_write(int fd, const void *buf, size_t len) {
       return len;
 
     case FD_FB:
-      fb_write(buf, files[fd].offset, bytes_to_write);
+      fb_write(buf, f->open_offset, bytes_to_write);
       break;
 
     default:
-      ramdisk_write(buf, file_table[files[fd].index].disk_offset + files[fd].offset, bytes_to_write);
+      ramdisk_write(buf, f->disk_offset + f->open_offset, bytes_to_write);
       break;
   }
 
-	files[fd].offset += bytes_to_write;
+	f->open_offset += bytes_to_write;
 
 	return bytes_to_write;
 }
 
 off_t fs_lseek(int fd, off_t offset, int whence) {
-	int new_offset = files[fd].offset;
-	int file_size = file_table[files[fd].index].size;
+  Finfo *f = file_table + fd;
+	int new_offset = f->open_offset;
+	int file_size = file_table[fd].size;
 	switch(whence) {
 		case SEEK_CUR: new_offset += offset; break;
 		case SEEK_SET: new_offset = offset; break;
@@ -125,7 +111,7 @@ off_t fs_lseek(int fd, off_t offset, int whence) {
 	else if(new_offset > file_size) {
 		new_offset = file_size;
 	}
-	files[fd].offset = new_offset;
+	f->open_offset = new_offset;
 
 	return new_offset;
 }
