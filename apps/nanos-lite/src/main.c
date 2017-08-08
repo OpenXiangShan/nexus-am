5ae01b3ec04c5e796e9e62595a74350ce24d9256
Zihao Yu <yuzihao@ict.ac.cn>
2017-08-08 17:25:52 +0800

    apps,nanos-lite,main: use macros to control functionailities

--- apps/nanos-lite/src/main.c
+++ apps/nanos-lite/src/main.c
@@ -1,5 +1,10 @@
 #include "common.h"
 
+/* Uncomment these macros to enable corresponding functionality. */
+#define HAS_ASYE
+#define HAS_FS
+#define HAS_PTE
+
 void init_mm(void);
 void init_device(void);
 void init_irq(void);
@@ -7,7 +12,9 @@ void init_fs(void);
 void load_prog(const char *);
 
 int main() {
+#ifdef HAS_PTE
   init_mm();
+#endif
 
   Log("'Hello World!' from Nanos-lite");
   Log("Build time: %s, %s", __TIME__, __DATE__);
@@ -19,10 +26,14 @@ int main() {
 
   init_device();
 
+#ifdef HAS_ASYE
   Log("Initializing interrupt/exception handler...");
   init_irq();
+#endif
 
+#ifdef HAS_FS
   init_fs();
+#endif
 
   load_prog("/bin/pal");
   load_prog("/bin/hello");
