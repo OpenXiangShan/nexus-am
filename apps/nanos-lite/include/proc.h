2a54148d10026196f79ed8c8ef0c36e524d274b0
Zihao Yu <yuzihao@ict.ac.cn>
2017-08-11 23:07:06 +0800

    apps,nanos-lite,proc: change stack size to 1MB

--- apps/nanos-lite/include/proc.h
+++ apps/nanos-lite/include/proc.h
@@ -4,7 +4,7 @@
 #include "common.h"
 #include "memory.h"
 
-#define STACK_SIZE (128 * PGSIZE)
+#define STACK_SIZE (256 * PGSIZE)
 
 typedef union {
   uint8_t stack[STACK_SIZE] PG_ALIGN;
@@ -12,7 +12,7 @@ typedef union {
     _RegSet *tf;
     _Protect as;
     uintptr_t cur_brk;
-    // we do not free memory, so use `max_brk' to determine whether to call mm_malloc()
+    // we do not free memory, so use `max_brk' to determine when to call _map()
     uintptr_t max_brk;
   };
 } PCB;
