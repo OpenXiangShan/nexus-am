c68d2e2c6553994f5857605ca3b7f88389a61bae
Yanyan Jiang <jiangyy@outlook.com>
2018-03-23 14:47:59 +0800

    large changes

--- am/am.h
+++ am/am.h
@@ -14,18 +14,39 @@
 # define NULL ((void *)0)
 #endif
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// =======================================================================
+// [0] Turing Machine
+// =======================================================================
+
 typedef struct _Area {
   void *start, *end;
 } _Area; 
+extern _Area _heap;
+
+void _putc(char ch);
+void _halt(int code);
+
+// =======================================================================
+// [1] I/O Extension (IOE)
+// =======================================================================
 
 typedef struct _Device {
   uint32_t id;
   const char *name;
-  uintptr_t (*read)(uintptr_t reg, size_t nmemb);
-  void (*write)(uintptr_t reg, size_t nmemb, uintptr_t data);
+  uintptr_t (*read)(uintptr_t reg, void *buf, size_t size);
+  void (*write)(uintptr_t reg, void *buf, size_t size);
 } _Device;
 
-typedef struct _RegSet _RegSet;
+void _ioe_init();
+_Device *_device(int n);
+
+// =======================================================================
+// [2] Asynchronous Extension (ASYE)
+// =======================================================================
 
 enum {
   _EVENT_NULL = 0,
@@ -36,45 +57,17 @@ enum {
   _EVENT_TRAP,
   _EVENT_SYSCALL,
 };
-
 typedef struct _Event {
   int event;
   uintptr_t cause, ref;
 } _Event;
-
-typedef struct _Protect {
-  _Area area; 
-  size_t pgsize;
-  void *ptr;
-} _Protect;
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-// =======================================================================
-// [0] Turing Machine
-// =======================================================================
-
-extern _Area _heap;
-void _putc(char ch);
-void _halt(int code);
-
-// =======================================================================
-// [1] I/O Extension (IOE)
-// =======================================================================
-
-void _ioe_init();
-_Device *_device(int n);
-
-// =======================================================================
-// [2] Asynchronous Extension (ASYE)
-// =======================================================================
+typedef struct _RegSet _RegSet;
 
 void _asye_init(_RegSet *(*l)(_Event ev, _RegSet *regs));
 _RegSet *_make(_Area kstack, void *entry, void *arg);
-void _trap();
-int _istatus(int enable);
+void _yield();
+void _set_intr(int enable);
+int _get_intr();
 
 // =======================================================================
 // [3] Protection Extension (PTE)
@@ -84,13 +77,18 @@ int _istatus(int enable);
 #define _PROT_READ   2
 #define _PROT_WRITE  4
 #define _PROT_EXEC   8
+typedef struct _Protect {
+  _Area area; 
+  size_t pgsize;
+  void *ptr;
+} _Protect;
+
 void _pte_init(void*(*pgalloc)(), void (*pgfree)(void*));
-void _protect(_Protect *p);
-void _release(_Protect *p);
-void _map(_Protect *p, void *va, void *pa, int prot);
-void *_query(_Protect *p, void *va, int *prot);
-void _unmap(_Protect *p, void *va);
-void _switch(_Protect *p);
+void _prot_create(_Protect *p);
+void _prot_destroy(_Protect *p);
+void _prot_switch(_Protect *p);
+void _map(_Protect *p, void *va, void *pa);
+void _protect(_Protect *p, void *va, int len, int prot);
 _RegSet *_umake(_Protect *p, _Area ustack, _Area kstack, void *entry, void *args);
 
 // =======================================================================
