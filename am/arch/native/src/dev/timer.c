ac71d9778e3c20f9cfd78f03aed23c6326338692
Zihao Yu <yuzihao@ict.ac.cn>
2018-03-25 19:16:36 +0800

    am,native: update IOE

--- am/arch/native/src/dev/timer.c
+++ am/arch/native/src/dev/timer.c
@@ -7,11 +7,23 @@ static struct timeval boot_time;
 
 
 size_t timer_read(uintptr_t reg, void *buf, size_t size) {
-  struct timeval now;
-  gettimeofday(&now, NULL);
-  long seconds = now.tv_sec - boot_time.tv_sec;
-  long useconds = now.tv_usec - boot_time.tv_usec;
-  return seconds * 1000 + (useconds + 500) / 1000;
+  switch (reg) {
+    case _DEVREG_TIMER_UPTIME: {
+      struct timeval now;
+      gettimeofday(&now, NULL);
+      long seconds = now.tv_sec - boot_time.tv_sec;
+      long useconds = now.tv_usec - boot_time.tv_usec;
+      _UptimeReg *uptime = (_UptimeReg *)buf;
+      uptime->hi = 0;
+      uptime->lo = seconds * 1000 + (useconds + 500) / 1000;
+      return sizeof(_UptimeReg);
+    }
+    case _DEVREG_TIMER_DATE: {
+      // TODO: use gmtime() to get date
+      return 0;
+    }
+  }
+  return 0;
 }
 
 void timer_init() {
