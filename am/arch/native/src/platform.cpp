eca97cdd35824891e4e6bc7b8e341ca7706cfd3e
Zihao Yu <yuzihao@ict.ac.cn>
2018-04-25 21:54:42 +0800

    am,arch,native,platform: add destructor

--- am/arch/native/src/platform.cpp
+++ am/arch/native/src/platform.cpp
@@ -1,36 +1,51 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include <klib.h>
+
+#define PMEM_SHM_FILE "/native-pmem"
+#define PMEM_SIZE (128 * 1024 * 1024) // 128MB
+#define PMEM_MAP_START (uintptr_t)0x100000
+#define PMEM_MAP_END   (uintptr_t)PMEM_SIZE
+#define PMEM_MAP_SIZE  (PMEM_MAP_END - PMEM_MAP_START)
 
 static int pmem_fd;
 static ucontext_t uc_example;
 
 static void init_platform() {
-  pmem_fd = shm_open("/native-pmem", O_RDWR | O_CREAT, 0700);
+  pmem_fd = shm_open(PMEM_SHM_FILE, O_RDWR | O_CREAT, 0700);
   assert(pmem_fd != -1);
-  size_t size = 128 * 1024 * 1024; // 128MB
-  ftruncate(pmem_fd, size);
+  ftruncate(pmem_fd, PMEM_SIZE);
 
-  uintptr_t start = 0x100000;
-  uintptr_t end = size;
-  start = (uintptr_t)mmap((void *)start, end - start, PROT_READ | PROT_WRITE | PROT_EXEC,
-      MAP_SHARED | MAP_FIXED, pmem_fd, start);
-  assert(start != (uintptr_t)-1);
+  void *ret = mmap((void *)PMEM_MAP_START, PMEM_MAP_SIZE, PROT_READ | PROT_WRITE | PROT_EXEC,
+      MAP_SHARED | MAP_FIXED, pmem_fd, PMEM_MAP_START);
+  assert(ret != (void *)-1);
 
-  _heap.start = (void *)(start + 4096);  // this is to skip the trap entry
-  _heap.end = (void *)end;
+  _heap.start = (void *)(PMEM_MAP_START + 4096);  // this is to skip the trap entry
+  _heap.end = (void *)PMEM_MAP_END;
 
   getcontext(&uc_example);
+}
+
+static void exit_platform() {
+  int ret = munmap((void *)PMEM_MAP_START, PMEM_MAP_SIZE);
+  assert(ret == 0);
+  close(pmem_fd);
+  ret = shm_unlink(PMEM_SHM_FILE);
+  assert(ret == 0);
 }
 
 class _Init {
   public : _Init() {
     init_platform();
   }
+  public : ~_Init() {
+    exit_platform();
+  }
 };
 
-static _Init _init;
+static _Init _init_platform;
 
 extern "C" {
 
@@ -49,6 +64,12 @@ void shm_munmap(void *va) {
 
 void get_example_uc(_RegSet *r) {
   memcpy(&r->uc, &uc_example, sizeof(uc_example));
+}
+
+// This dummy function will be called in trm.c.
+// The purpose of this dummy function is to let linker add this file to the object
+// file set. Without it, the constructor of @_init_platform will not be linked.
+void platform_dummy() {
 }
 
 }
--- am/arch/native/src/trm.c
+++ am/arch/native/src/trm.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+extern void platform_dummy();
 void _trm_init() {
+  platform_dummy();
 }
 
 void _putc(char ch) {
