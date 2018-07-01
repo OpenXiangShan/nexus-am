7d9f0c98aace86904d01fcef8f37873bf9756b6c
141242068-ouxianfei <141242068@smail.nju.edu.cn>
2018-07-01 19:45:13 +0800

    Added arch mips32-nemu

---
+++ am/arch/mips32-nemu/include/arch.h
@@ -0,0 +1,20 @@
+#ifndef __ARCH_H__
+#define __ARCH_H__
+
+struct _RegSet {
+	uint32_t at,
+	v0,v1,
+	a0,a1,a2,a3,
+	t0,t1,t2,t3,t4,t5,t6,t7,t8,t9,
+	s0,s1,s2,s3,s4,s5,s6,s7,
+	k0,k1,
+	gp,sp,fp,ra,
+	epc, cause, status, badvaddr;
+};
+
+#define GPR1 a0
+#define GPR2 a1
+#define GPR3 a2
+#define GPR4 a3
+
+#endif
--- tests/asyetest/main.c
+++ tests/asyetest/main.c
@@ -4,6 +4,10 @@
 
 int ntraps = 0;
 _RegSet* handler(_Event ev, _RegSet *regs) {
+  for(const char *p = "intr:"; *p; p++) _putc(*p);
+  _putc(_intr_read() ? '1' : '0');
+  _putc('\n');
+
   switch (ev.event) {
     case _EVENT_IRQ_TIMER:
       printf(".");
@@ -28,13 +32,39 @@ _RegSet* handler(_Event ev, _RegSet *regs) {
   return regs;
 }
 
+#define _STR(x) _VAL(x)
+#define _VAL(x) #x
+
+#define MFC0(dst, src, sel) \
+asm volatile("mfc0 %0, $"_STR(src)", %1\n\t":"=r"(dst):"i"(sel))
+
+void read_write_cp0() {
+  int reg;
+#define CP0_OUT(src, sel) \
+  reg = 0x12345678; \
+  MFC0(reg, src, sel); \
+  printf("CP0:{" #src ", " #sel "}=%x\n", reg);
+  CP0_OUT(8, 0)
+  CP0_OUT(9, 0)
+  CP0_OUT(9, 1)
+  CP0_OUT(11, 0)
+  CP0_OUT(12, 0)
+  CP0_OUT(13, 0)
+  CP0_OUT(14, 0)
+}
+
 int main(){
   _ioe_init();
   _asye_init(handler);
+  read_write_cp0();
   assert(!_intr_read());
   _intr_write(1);
   //_make(_heap, void*)main, 0);
+  printf("intr:%x\n", _intr_read());
   while (1) {
+	for(int i = 0; i < 10; i++)
+	  printf("-");
+	printf(":yield, intr:%x\n", _intr_read());
     _yield();
   }
   return 0;
