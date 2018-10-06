ef2591af72ff4e19a7f5f39d10922a69eb00200f
Zihao Yu <yuzihao@ict.ac.cn>
2018-10-06 16:34:50 +0800

    typing,game: process all key events during a frame
    * also when we check _KEY_NONE, ignore the keydown field

--- apps/typing/keyboard.c
+++ apps/typing/keyboard.c
@@ -39,16 +39,16 @@ int last_key_code(void) {
 #define KEYDOWN_MASK 0x8000
 bool keyboard_event() {
   int keycode = read_key();
-  if (keycode == _KEY_NONE) return false;
+  if ((keycode & ~KEYDOWN_MASK) == _KEY_NONE) return false;
 
-  if((keycode & KEYDOWN_MASK) != 0){
+  if ((keycode & KEYDOWN_MASK) != 0) {
     key_code = keycode & ~KEYDOWN_MASK;
     press_key(key_code);
     return true;
   }
-  else{
-    for(int i = 0; i < 26; i++){
-      if(keycode == letter_code[i]){
+  else {
+    for (int i = 0; i < 26; i++) {
+      if (keycode == letter_code[i]) {
         release_key(i);
       }
     }
