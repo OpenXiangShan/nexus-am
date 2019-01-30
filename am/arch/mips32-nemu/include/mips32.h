e2ba27ca00e4105fe392afc0760fe4a9c1eeccb5
Zihao Yu <yuzihao@ict.ac.cn>
2019-01-30 17:40:45 +0800

    mips32-nemu,mips32.h: map MMIO to 0xa0000000

--- am/arch/mips32-nemu/include/mips32.h
+++ am/arch/mips32-nemu/include/mips32.h
@@ -5,13 +5,15 @@
 
 #include <arch.h>
 
-static inline uint8_t  inb(uintptr_t addr) { return *(volatile uint8_t  *)addr; }
-static inline uint16_t inw(uintptr_t addr) { return *(volatile uint16_t *)addr; }
-static inline uint32_t inl(uintptr_t addr) { return *(volatile uint32_t *)addr; }
+#define MMIO_OFFSET(addr) ((uintptr_t)0xa0000000 + addr)
 
-static inline void outb(uintptr_t addr, uint8_t  data) { *(volatile uint8_t  *)addr = data; }
-static inline void outw(uintptr_t addr, uint16_t data) { *(volatile uint16_t *)addr = data; }
-static inline void outl(uintptr_t addr, uint32_t data) { *(volatile uint32_t *)addr = data; }
+static inline uint8_t  inb(uintptr_t addr) { return *(volatile uint8_t  *)MMIO_OFFSET(addr); }
+static inline uint16_t inw(uintptr_t addr) { return *(volatile uint16_t *)MMIO_OFFSET(addr); }
+static inline uint32_t inl(uintptr_t addr) { return *(volatile uint32_t *)MMIO_OFFSET(addr); }
+
+static inline void outb(uintptr_t addr, uint8_t  data) { *(volatile uint8_t  *)MMIO_OFFSET(addr) = data; }
+static inline void outw(uintptr_t addr, uint16_t data) { *(volatile uint16_t *)MMIO_OFFSET(addr) = data; }
+static inline void outl(uintptr_t addr, uint32_t data) { *(volatile uint32_t *)MMIO_OFFSET(addr) = data; }
 
 #endif
 
