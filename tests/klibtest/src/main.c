d442d20a326eafa999e4ced54eb1e7fed0ab16f8
Zihao Yu <yuzihao@ict.ac.cn>
2019-02-10 16:38:28 +0800

    klibtest: refactor

--- tests/klibtest/src/main.c
+++ tests/klibtest/src/main.c
@@ -5,10 +5,9 @@ void printk_test();
 void memory_test();
 
 int main() {
-  printk_test();
+  printf("Test start!\n");
+  //printk_test();
   memory_test();
-  //printk("%03dd\n",0);
-  // Finally reaches here.
-  int volatile x = 0;
-  assert(x != x);
+  printf("Test end!\n");
+  return 0;
 }
