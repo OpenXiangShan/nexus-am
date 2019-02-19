859c42d41f4f47fe66ad3fadde1d30a79330937a
Zihao Yu <yuzihao@ict.ac.cn>
2019-02-19 14:02:13 +0800

    am,mips32-nemu: better arrangement for _Context

--- am/include/arch/mips32-nemu.h
+++ am/include/arch/mips32-nemu.h
@@ -2,16 +2,18 @@
 #define __ARCH_H__
 
 struct _Context {
-  struct _AddressSpace *prot;
-  uint32_t gpr[31];
+  union {
+    struct _AddressSpace *prot;
+    uint32_t gpr[32];
+  };
   uint32_t lo, hi;
   uint32_t cause, status, epc;
 };
 
-#define GPR1 gpr[3]
-#define GPR2 gpr[4]
-#define GPR3 gpr[5]
-#define GPR4 gpr[6]
-#define GPRx gpr[1]
+#define GPR1 gpr[4]
+#define GPR2 gpr[5]
+#define GPR3 gpr[6]
+#define GPR4 gpr[7]
+#define GPRx gpr[2]
 
 #endif
