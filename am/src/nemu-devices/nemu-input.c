aa1d9c091a644885b9c2483c2142f548c6968c70
Zihao Yu <yuzihao@ict.ac.cn>
2019-05-07 17:24:33 +0800

    am,x86-nemu: avoid symbol overwritten by nanos

--- am/src/nemu-devices/nemu-input.c
+++ am/src/nemu-devices/nemu-input.c
@@ -4,7 +4,7 @@
 
 #define KEYDOWN_MASK 0x8000
 
-size_t input_read(uintptr_t reg, void *buf, size_t size) {
+size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
