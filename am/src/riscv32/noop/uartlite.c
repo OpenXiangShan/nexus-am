350fd48b7b40f028c6cb7a6a585b915afe7de4ce
Zihao Yu <yuzihao@ict.ac.cn>
2019-09-09 09:56:43 +0800

    riscv32-noop,input: simulate keystrobes with UART
    * UART can only transmit character data, but not keystrobes.
    * To do this, we record the last data UART received, By default we
      generate a keydown event. A keyup event is generated when
        * UART receive a different data from the last one
        * or no data is received in 0.5s
    * This is not perfect. Key combination can not be detected since a keyup
      event is generated before another keydown event. But it is enough to
      play PAL with UART.

--- am/src/riscv32/noop/uartlite.c
+++ am/src/riscv32/noop/uartlite.c
@@ -8,6 +8,7 @@
 
 #define UARTLITE_RST_FIFO 0x03
 #define UARTLITE_TX_FULL  0x08
+#define UARTLITE_RX_VALID 0x01
 
 void __am_init_uartlite(void) {
   outb(UARTLITE_MMIO + UARTLITE_CTRL_REG, UARTLITE_RST_FIFO);
@@ -18,4 +19,10 @@ void __am_uartlite_putchar(char ch) {
 
   while (inb(UARTLITE_MMIO + UARTLITE_STAT_REG) & UARTLITE_TX_FULL);
   outb(UARTLITE_MMIO + UARTLITE_TX_FIFO, ch);
+}
+
+int __am_uartlite_getchar() {
+  if (inb(UARTLITE_MMIO + UARTLITE_STAT_REG) & UARTLITE_RX_VALID)
+    return inb(UARTLITE_MMIO + UARTLITE_RX_FIFO);
+  return 0;
 }
