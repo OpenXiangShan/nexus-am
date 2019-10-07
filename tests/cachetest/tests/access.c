f3850d99f20eaa029ee8ff0c5302b4bc964a92a3
Zihao Yu <yuzihao@ict.ac.cn>
2019-10-07 22:54:55 +0800

    test,cachetest,access: add conflict test

--- tests/cachetest/tests/access.c
+++ tests/cachetest/tests/access.c
@@ -1,12 +1,21 @@
 #include "klib.h"
 
-int ans[64] = {0};
+#define N 64
+#define CACHE_SIZE (32 * 1024)
+int ans[2 * CACHE_SIZE / sizeof(int)] = {0};
 
 int main() {
   int i = 0;
-  for (i = 0; i < 64; i++)
-    ans[i] = 64 - i;
-  for (i = 0; i < 64; i++)
-    assert(ans[i] == 64 - i);
+  for (i = 0; i < N; i++)
+    ans[i] = i;
+
+  int start = CACHE_SIZE / sizeof(int);
+  for (i = 0; i < N; i++)
+    ans[start + i] = start + i;
+
+  for (i = 0; i < N; i++) {
+    assert(ans[i] == i);
+    assert(ans[start + i] == start + i);
+  }
   return 0;
 }
