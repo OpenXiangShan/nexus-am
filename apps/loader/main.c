ae90ed110f9b16831ca9811bcd6ce3cd7a38df8b
Wang Huizhe <wanghuizhe@ict.ac.cn>
2019-12-23 10:07:35 +0800

    riscv64-asic: adjust tests

--- apps/loader/main.c
+++ apps/loader/main.c
@@ -6,7 +6,7 @@ extern uint8_t image;
 
 #define ADDR(x) (uint32_t)(uintptr_t)(x)
 int main() {
-  printf("Hello world! Build time: %s %s\n", __DATE__, __TIME__);
+  printf("main: %s %s\n", __DATE__, __TIME__);
 
   Elf64_Ehdr *elf = (void *)&image;
   Elf64_Phdr *ph, *eph;
@@ -19,7 +19,10 @@ int main() {
   for (; ph < eph; ph ++) {
     if (ph->p_type == PT_LOAD) {
       printf("loading to memory region [0x%08x, 0x%08x)\n", ADDR(ph->p_vaddr), ADDR(ph->p_vaddr + ph->p_filesz));
-      memcpy((void *)ph->p_vaddr, &image + ph->p_offset, ph->p_filesz);
+      for (uint32_t *p = (void *)ph->p_vaddr, *q = (void *)(&image + ph->p_offset); p < (uint32_t *)(ph->p_vaddr + ph->p_filesz); p++) {
+          *p = *q++;
+      }
+      //memcpy((void *)ph->p_vaddr, &image + ph->p_offset, ph->p_filesz);
 
       // now we still do not have any code in icache
       //if (ph->p_flags & PF_X) {
@@ -29,12 +32,20 @@ int main() {
 
       printf("zeroing memory region [0x%08x, 0x%08x)\n",
           ADDR(ph->p_vaddr + ph->p_filesz), ADDR(ph->p_vaddr + ph->p_memsz));
-      memset((void *)ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
+
+      //////memset((void *)ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
+      uint32_t sz = (ph->p_filesz >> 2) << 2;
+      for (uint32_t *p = (void *)(ph->p_vaddr + sz); p < (uint32_t *)(ph->p_vaddr + ph->p_memsz); p++) {
+          *p = 0;
+      }
     }
   }
 
+  printf("Hello\n");
+
   void (*f)(void) = (void *)(uintptr_t)elf->e_entry;
 
+  /*
   int i;
   for (i = 0; i < 10; i ++) {
     printf("instr[%d] = 0x%08x\n", i, ((uint32_t *)(void*)f)[i]);
@@ -43,6 +54,7 @@ int main() {
   for (i = 0; i < 10; i ++) {
     printf("do_reset[%d] = 0x%08x\n", i, ((uint32_t *)((void*)f + 0x200))[i]);
   }
+  */
 
   printf("jump to 0x%x...\n", ADDR(f));
   f(); // here we go
--- tests/cachetest/tests/sdram.c
+++ tests/cachetest/tests/sdram.c
@@ -1,9 +1,10 @@
 #include "klib.h"
 
+extern uint32_t _heap_start;
 int main() {
   volatile uint32_t *p;
   uint32_t *p_start = (uint32_t *)(uintptr_t)(0x80100000); // _heap.end
-  uint32_t *p_end = (uint32_t *)(uintptr_t)(0x80400000); // _heap.end
+  uint32_t *p_end = (uint32_t *)(uintptr_t)  (0x84000000); // _heap.end
 
   printf("Testing SDRAM interval [%x, %x)...\n",
       (uint32_t)(uintptr_t)p_start, (uint32_t)(uintptr_t)p_end);
@@ -11,7 +12,7 @@ int main() {
 
   for (p = p_start; p < p_end; p ++) {
     uint32_t data = (uint32_t)(uintptr_t)p;
-    if ((data & 0xfffff) == 0) {
+    if ((data & 0xffff) == 0) {
       printf("finish writing address %x with data %x...\n", data, data);
     }
     *p = data;
@@ -22,7 +23,7 @@ int main() {
   for (p = p_start; p < p_end; p ++) {
     uint32_t data = *p;
     uint32_t right = (uint32_t)(uintptr_t)p;
-    if ((right & 0xfffff) == 0) {
+    if ((right & 0xffff) == 0) {
       printf("finish checking address %x...\n", right);
     }
     if (data != right) {
