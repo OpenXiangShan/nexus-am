7ad5613dda61f163e75945e0a0accb78bf0b64e6
Yanyan Jiang <jiangyy@outlook.com>
2020-01-23 14:29:10 +0000

    a better loader

--- tests/hello/hello.c
+++ tests/hello/hello.c
@@ -1,13 +1,7 @@
 #include <am.h>
 #include <klib.h>
 
-
-void foo() {
-  while (1) _putc('.');
-}
-
-
 int main(const char *args) {
-  printf("args = %s\n", args);
+  printf("Hello with args=\"%s\"\n", args);
   return 0;
 }
