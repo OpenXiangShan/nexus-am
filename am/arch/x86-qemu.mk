efc7fb272c318180ebbc8dfaa82fbbf5380e77c6
Yanyan Jiang <jiangyy@outlook.com>
2020-02-02 15:06:25 +0800

    remove empty line

--- am/arch/x86-qemu.mk
+++ am/arch/x86-qemu.mk
@@ -15,7 +15,6 @@ image:
 	@echo + CREATE "->" $(BINARY_REL)
 	@( cat $(AM_HOME)/am/src/x86/qemu/boot/mbr; head -c 1024 /dev/zero; cat $(BINARY).o ) > $(BINARY)
 
-
 run:
 	@( echo -n $(mainargs); ) | dd if=/dev/stdin of=$(BINARY) bs=512 count=2 seek=1 conv=notrunc status=none
 	@qemu-system-i386 -serial stdio -machine accel=kvm:tcg -smp "$(smp)" -drive format=raw,file=$(BINARY)
