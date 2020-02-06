b5db2daab5d45909446b83ceb80e3e1ba5543f13
Zihao Yu <yuzihao@ict.ac.cn>
2020-02-06 23:03:13 +0800

    libs,klib,stdlib: enhance atoi()
    * skip leading space and finish with non-digit character

--- libs/klib/src/stdlib.c
+++ libs/klib/src/stdlib.c
@@ -19,7 +19,8 @@ int abs(int x) {
 
 int atoi(const char* nptr) {
   int x = 0;
-  while (*nptr != '\0') {
+  while (*nptr == ' ') { nptr ++; }
+  while (*nptr >= '0' && *nptr <= '9') {
     x = x * 10 + *nptr - '0';
     nptr ++;
   }
