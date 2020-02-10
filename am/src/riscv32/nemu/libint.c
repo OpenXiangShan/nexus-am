aca9a5dd6d28b31f631dc808cf501cddb6195a78
Zihao Yu <yuzihao@ict.ac.cn>
2020-02-10 13:22:36 +0800

    riscv32-nemu: add integer library functions to pass linking
    * they are called in __udivmoddi4(), which is used with uint64_t
      division in klib/src/stdio.c

---
+++ am/src/riscv32/nemu/libint.c
@@ -0,0 +1,14 @@
+#include <klib.h>
+
+// for more details, refer to
+// https://gcc.gnu.org/onlinedocs/gccint/Integer-library-routines.html
+
+// only use for linking
+
+int __clzsi2 (unsigned int a) {
+  assert(0);
+}
+
+int __ctzsi2 (unsigned int a) {
+  assert(0);
+}
