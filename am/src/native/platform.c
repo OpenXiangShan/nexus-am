ec7a5ed98879b9ca7ac4f3684074bd344d4dd329
Zihao Yu <yuzihao@ict.ac.cn>
2020-02-13 22:22:43 +0800

    native,platform: fix silly bug for mmap_prot

--- am/src/native/platform.c
+++ am/src/native/platform.c
@@ -173,9 +173,9 @@ void __am_shm_mmap(void *va, void *pa, int prot) {
   int mmap_prot = PROT_NONE;
   // we do not support executable bit, so mark
   // all readable pages executable as well
-  if (prot | _PROT_READ) mmap_prot |= PROT_READ | PROT_EXEC;
-  if (prot | _PROT_WRITE) mmap_prot |= PROT_WRITE;
-  void *ret = mmap(va, __am_pgsize, PROT_READ | PROT_WRITE | PROT_EXEC,
+  if (prot & _PROT_READ) mmap_prot |= PROT_READ | PROT_EXEC;
+  if (prot & _PROT_WRITE) mmap_prot |= PROT_WRITE;
+  void *ret = mmap(va, __am_pgsize, mmap_prot,
       MAP_SHARED | MAP_FIXED, pmem_fd, (uintptr_t)(pa - pmem));
   assert(ret != (void *)-1);
 }
