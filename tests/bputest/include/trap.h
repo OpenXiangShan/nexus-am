3f283446e26e0a4b4695e9d750efdbd19e3c3c71
ZhangZifei <1773908404@qq.com>
2020-07-13 13:53:48 +0800

    bputest: create bputest dir

---
+++ tests/bputest/include/trap.h
@@ -0,0 +1,13 @@
+#ifndef __TRAP_H__
+#define __TRAP_H__
+
+#include <am.h>
+#include <klib.h>
+#include <klib-macros.h>
+
+__attribute__((noinline))
+void nemu_assert(int cond) {
+  if (!cond) _halt(1);
+}
+
+#endif
