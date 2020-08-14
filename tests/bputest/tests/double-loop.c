95b986dae5cb8131f79c8dea807fab835da174a0
Lingrui98 <goulingrui15@gmail.com>
2020-08-14 13:58:19 +0800

    update double-loop

--- tests/bputest/tests/double-loop.c
+++ tests/bputest/tests/double-loop.c
@@ -1,21 +1,31 @@
 # define OutLoopNum 100
-# define InnerLoopNum 2
-# define RESULT OutLoopNum*InnerLoopNum*2
+# define InnerLoopNum 50
+# define RESULT OutLoopNum*inner
 #include "trap.h"
 
-int add() {
-    register int a=0,b=0;
+int loop(int inner) {
+    register int a=0, b=0;
     for (register int i = 0; i < OutLoopNum; i++) {
-        for (register int j = 0; j < InnerLoopNum; j++) {
-            b++;
-            a++;
+        for (register int j = 0; j < inner; j++) {
+            b = a + 1;
+            a = b + 1;
         }
     }
-    return a+b;
+    return a / 2;
 }
 
-int main () {
-    register int r = add();
-    nemu_assert(r == RESULT);
+int tonum(const char s) {
+    return (int)(s - '0');
+}
+
+int atoi(const char *s) {
+    return tonum(s[0]) * 10 + tonum(s[1]);
+}
+
+int main (const char *args) {
+    int inner = atoi(args);
+    register int i = loop(inner);
+    // printf("inner loop num is %d\n", inner);
+    nemu_assert(i == RESULT);
     return 0;
 }
