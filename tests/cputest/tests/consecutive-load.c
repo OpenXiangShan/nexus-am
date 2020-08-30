0778932005e5af715760881b64212966705e9efd
Allen <njuallen@foxmail.com>
2020-08-30 13:14:36 +0800

    cputest: consective-load/store forward, use it.

--- tests/cputest/tests/consecutive-load.c
+++ tests/cputest/tests/consecutive-load.c
@@ -1,10 +1,10 @@
 #include "trap.h"
 
 #define SIZE 4096
-volatile unsigned char mem[SIZE] = { 0x0 };
+volatile int mem[SIZE] = { 0x0 };
 
 int main() {
-	unsigned i;
+	int i;
 
 	int sum = 0;
 	for(i = 0; i < SIZE; i ++) {
--- tests/cputest/tests/load-store-forward.c
+++ tests/cputest/tests/load-store-forward.c
@@ -1,10 +1,10 @@
 #include "trap.h"
 
 #define SIZE 4096
-unsigned int mem[SIZE] = { 0x0 };
+volatile int mem[SIZE] = { 0x0 };
 
 int main() {
-	unsigned i;
+	int i;
 
 	for(i = 1; i < SIZE; i ++) {
 		mem[i] = mem[i - 1] + 1;
