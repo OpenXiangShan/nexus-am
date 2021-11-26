9cada1e8d418c9fed66c43cc14f040042249c4b5
Li Qianruo <liqianruo20s@ict.ac.cn>
2021-11-26 15:20:53 +0800

    Temp Commit

---
+++ tests/triggertest/main.c
@@ -0,0 +1,14 @@
+#include <am.h>
+#include <klib.h>
+
+int identify_triggers(void);
+
+int main() {
+  if(identify_triggers()) return 1;
+  return 0;
+}
+
+int identify_triggers(void){
+  printf("Checking triggers.../n");
+  //
+}
