014b8df7c6168f18457e41693671972209eb3d1f
Jin Yue <jinyue20s@ict.ac.cn>
2022-04-13 12:15:54 +0800

    loader: use linux.bin as default ELF_PATH

--- apps/loader/main.c
+++ apps/loader/main.c
@@ -1,95 +1,30 @@
-// /*
-//  * @Author: your name
-//  * @Date: 2021-10-20 20:37:55
-//  * @LastEditTime: 2021-10-20 20:37:56
-//  * @LastEditors: your name
-//  * @Description: In User Settings Edit
-//  * @FilePath: /nexus-am/apps/loader/main.c
-//  */
-// #include <am.h>
-// #include <klib.h>
-// #include <elf.h>
-
-// extern uint8_t image;
-
-// #define ADDR(x) (uint32_t)(uintptr_t)(x)
-// #define ADDRPTR(x) (void *)(uintptr_t)(ADDR(x))
-// int main() {
-
-//   Elf64_Ehdr *elf = (void *)&image;
-//   Elf64_Phdr *ph, *eph;
-
-//   /* Load each program segment */
-//   ph = (void *)elf + elf->e_phoff;
-//   eph = ph + elf->e_phnum;
-//   for (; ph < eph; ph ++) {
-//     if (ph->p_type == PT_LOAD) {
-//       memcpy(ADDRPTR(ph->p_vaddr), &image + ADDR(ph->p_offset), ADDR(ph->p_filesz));
-
-//       // now we still do not have any code in icache
-//       //if (ph->p_flags & PF_X) {
-//       //  asm volatile("fence.i");
-//       //  Log("code");
-//       //}
-
-//       //printf("zeroing memory region [0x%08x, 0x%08x)\n",
-//       //    ADDR(ph->p_vaddr) + ADDR(ph->p_filesz), ADDR(ph->p_vaddr) + ADDR(ph->p_memsz));
-//       memset(ADDRPTR(ph->p_vaddr) + ADDR(ph->p_filesz), 0, ADDR(ph->p_memsz) - ADDR(ph->p_filesz));
-//     }
-//   }
-
-//   /* Check each program segment */
-// #if 0
-//   int wrong_time = 0;
-//   ph = (void *)elf + elf->e_phoff;
-//   eph = ph + elf->e_phnum;
-//   for (; ph < eph; ph ++) {
-//     if (ph->p_type == PT_LOAD) {
-//       printf("checking memory region [0x%08x, 0x%08x)\n", ADDR(ph->p_vaddr), ADDR(ph->p_vaddr + ph->p_filesz));
-//       int i;
-//       for (i = 0; i < ph->p_filesz; i ++) {
-//         uint8_t membyte = ((uint8_t *)ph->p_vaddr)[i];
-//         uint8_t flashbyte = (&image)[ph->p_offset + i];
-//         if (membyte != flashbyte) {
-//           wrong_time ++;
-//           printf("%8dth wrong byte detect at address 0x%08x, right = %02x, wrong = %02x, diff = %02x\n",
-//               wrong_time, ADDR(ph->p_vaddr + i), flashbyte, membyte, flashbyte ^ membyte);
-//         }
-//       }
-//     }
-//   }
-// #endif
-
-//   void (*f)(void) = (void *)(uintptr_t)elf->e_entry;
-
-//   //int i;
-//   //for (i = 0; i < 2; i ++) {
-//   //  printf("%02x: instr[%d] = 0x%08x\n", i * 4, i, ((uint32_t *)(void*)f)[i]);
-//   //}
-
-//   //printf("jump to 0x%x...\n", ADDR(f));
-//   f(); // here we go
-//   return 0;
-// }
-
 #include "klib.h"
 
-extern uint8_t image;
+extern uint8_t image, image_end, ram_start;
 
-int a[10] = {};
+inline void set_ram_executable() {
+#ifdef __ISA_RISCV64__
+  asm volatile("csrr  a0, 0x3a0");
+  asm volatile("li    a1, (0x8f << 16)");
+  asm volatile("or    a0, a0, a1");
+  asm volatile("csrw  0x3a0, a0");
+#endif
+}
 
-uint32_t* code = (uint32_t *)0x80000000;
-
-void test(uint8_t *image) {
-  printf("copy code...\n");
-  memcpy(code, image, 7776);
+void test(uint8_t *image_start, uint8_t *image_end) {
+  printf("copy ram_start...\n");
+  uint8_t *copy_dest = &ram_start;
+  memcpy(copy_dest, image_start, image_end - image_start);
+  printf("copy finish...\n");
 
 #ifdef __ISA_RISCV64__
   asm volatile("fence.i");
 #endif
 
-  void (*f)(void) = (void *)code;
-  printf("jump to  code...\n");
+  void (*f)(void) = (void *)copy_dest;
+  printf("set ram executable\n");
+  set_ram_executable();
+  printf("jump to  ram_start...\n");
   f();
   //printf("ret = %d\n", ret);
 }
@@ -98,11 +33,7 @@ int main() {
 #ifdef __ISA_NATIVE__
 # error Can not run with native, since data segment is not executable.
 #endif
-  for (int i = 0; i < 10; i ++) {
-    a[i] = i + 1;
-  }
-
-  test(&image);
+  test(&image, &image_end);
 
   return 0;
 }
