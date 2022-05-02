1677994c18f8c7c000936dfcb74b2c9c643db02a
William Wang <zeweiwang@outlook.com>
2022-05-02 17:45:40 +0800

    apps: do not set pmpcfg in loader

--- apps/loader/main.c
+++ apps/loader/main.c
@@ -2,15 +2,6 @@
 
 extern uint8_t image, image_end, ram_start;
 
-inline void set_ram_executable() {
-#ifdef __ISA_RISCV64__
-  asm volatile("csrr  a0, 0x3a0");
-  asm volatile("li    a1, (0x8f << 16)");
-  asm volatile("or    a0, a0, a1");
-  asm volatile("csrw  0x3a0, a0");
-#endif
-}
-
 void test(uint8_t *image_start, uint8_t *image_end) {
   printf("copy ram_start...\n");
   uint8_t *copy_dest = &ram_start;
@@ -22,9 +13,7 @@ void test(uint8_t *image_start, uint8_t *image_end) {
 #endif
 
   void (*f)(void) = (void *)copy_dest;
-  printf("set ram executable\n");
-  set_ram_executable();
-  printf("jump to  ram_start...\n");
+  printf("jump to ram_start...\n");
   f();
   //printf("ret = %d\n", ret);
 }
