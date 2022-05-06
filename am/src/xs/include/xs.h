0d39fcf582e55c9972d7a32ae9792b5403a797c8
William Wang <zeweiwang@outlook.com>
2022-05-06 20:05:55 +0800

    southlake: fix INTR_GEN fake device addr

--- am/src/xs/include/xs.h
+++ am/src/xs/include/xs.h
@@ -14,8 +14,8 @@
 #define PLIC_BASE_ADDR         (0x3c000000UL)
 #elif defined(__ARCH_RISCV64_XS_SOUTHLAKE) || defined(__ARCH_RISCV64_XS_SOUTHLAKE_FLASH)
 #define INTR_GEN_ADDR          (0x1f10060000UL)
-#define INTR_RANDOM            (0x1f00060008UL)
-#define INTR_RANDOM_MASK       (0x1f00060010UL)
+#define INTR_RANDOM            (0x1f10060008UL)
+#define INTR_RANDOM_MASK       (0x1f10060010UL)
 #define PLIC_BASE_ADDR         (0x1f0c000000UL)
 #endif
 
