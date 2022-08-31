51e329c7536dbb643c22e455d680b1b775449bd3
William Wang <zeweiwang@outlook.com>
2022-08-31 16:42:32 +0800

    riscv64: add LINUX_GNU_TOOLCHAIN parameter
    Add LINUX_GNU_TOOLCHAIN=1 in make command to use riscv64-linux-gnu-
    toolchain

--- am/arch/isa/riscv64.mk
+++ am/arch/isa/riscv64.mk
@@ -1,5 +1,10 @@
+ifeq ($(LINUX_GNU_TOOLCHAIN),1)
+CROSS_COMPILE := riscv64-linux-gnu-
+COMMON_FLAGS  := -fno-pic -march=rv64gc -mcmodel=medany
+else
 CROSS_COMPILE := riscv64-unknown-linux-gnu-
 COMMON_FLAGS  := -fno-pic -march=rv64gc_zba_zbb_zbc_zbs_zbkb_zbkc_zbkx_zknd_zkne_zknh_zkr_zksed_zksh_zkt -mcmodel=medany
+endif
 CFLAGS        += $(COMMON_FLAGS) -static
 ASFLAGS       += $(COMMON_FLAGS) -O0
 LDFLAGS       += -melf64lriscv
