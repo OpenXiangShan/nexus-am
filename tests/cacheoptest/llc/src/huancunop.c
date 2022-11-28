8f5f85de8361a7de92a4222ff306c975e3b0707a
Gravel Cai <37858537+gravelcai@users.noreply.github.com>
2022-11-28 17:39:36 +0800

    tests: enable llcop test for southlake (#25)
    * rewrite the llcop test program.
    
    * add two driver programs:
    	make_region_clean()
    	make_region_flush()
    
    add two tests:
    	test_clean()
    	test_flush()
    
    * complete llcop test.
    
    	* pass following tests:
    	*  test_invalid, test_clean, test_flush
    	*  make_region_invalid, make_region_clean, make_region_flush

--- tests/cacheoptest/llc/src/huancunop.c
+++ tests/cacheoptest/llc/src/huancunop.c
@@ -22,46 +22,46 @@ void wait_for_ready() {
 }
 
 void make_invalid(uint64_t paddr) {
-  *(uint64_t*)(CACHE_CTRL_BASE + CTRL_TAG_OFFSET) = get_tag(paddr);
-  *(uint64_t*)(CACHE_CTRL_BASE + CTRL_SET_OFFSET) = get_set(paddr);
-  (*(uint64_t*)CACHE_CMD_BASE) = CMD_CMO_INV;
+  *(volatile uint64_t*)(CACHE_CTRL_BASE + CTRL_TAG_OFFSET) = get_tag(paddr);
+  *(volatile uint64_t*)(CACHE_CTRL_BASE + CTRL_SET_OFFSET) = get_set(paddr);
+  *(volatile uint64_t*)CACHE_CMD_BASE = CMD_CMO_INV;
 }
 
 void make_clean(uint64_t paddr) {
-  *(uint64_t*)(CACHE_CTRL_BASE + CTRL_TAG_OFFSET) = get_tag(paddr);
-  *(uint64_t*)(CACHE_CTRL_BASE + CTRL_SET_OFFSET) = get_set(paddr);
-  (*(uint64_t*)CACHE_CMD_BASE) = CMD_CMO_CLEAN;
+  *(volatile uint64_t*)(CACHE_CTRL_BASE + CTRL_TAG_OFFSET) = get_tag(paddr);
+  *(volatile uint64_t*)(CACHE_CTRL_BASE + CTRL_SET_OFFSET) = get_set(paddr);
+  *(volatile uint64_t*)CACHE_CMD_BASE = CMD_CMO_CLEAN;
 }
 
 void make_flush(uint64_t paddr) {
-  *(uint64_t*)(CACHE_CTRL_BASE + CTRL_TAG_OFFSET) = get_tag(paddr);
-  *(uint64_t*)(CACHE_CTRL_BASE + CTRL_SET_OFFSET) = get_set(paddr);
-  (*(uint64_t*)CACHE_CMD_BASE) = CMD_CMO_FLUSH;
+  *(volatile uint64_t*)(CACHE_CTRL_BASE + CTRL_TAG_OFFSET) = get_tag(paddr);
+  *(volatile uint64_t*)(CACHE_CTRL_BASE + CTRL_SET_OFFSET) = get_set(paddr);
+  *(volatile uint64_t*)CACHE_CMD_BASE = CMD_CMO_FLUSH;
 }
 
 void make_region_invalid(uint64_t start_paddr, uint64_t size_in_byte) {
   for(uint64_t current_paddr = start_paddr; current_paddr < (start_paddr + size_in_byte); current_paddr += CACHE_LINE_SIZE_BYTE) {
     wait_for_ready();
-    *(uint64_t*)(CACHE_CTRL_BASE + CTRL_TAG_OFFSET) = get_tag(current_paddr);
-    *(uint64_t*)(CACHE_CTRL_BASE + CTRL_SET_OFFSET) = get_set(current_paddr);
-    (*(uint64_t*)CACHE_CMD_BASE) = CMD_CMO_INV;
+    *(volatile uint64_t*)(CACHE_CTRL_BASE + CTRL_TAG_OFFSET) = get_tag(current_paddr);
+    *(volatile uint64_t*)(CACHE_CTRL_BASE + CTRL_SET_OFFSET) = get_set(current_paddr);
+    *(volatile uint64_t*)CACHE_CMD_BASE = CMD_CMO_INV;
   }
 }
 
 void make_region_clean(uint64_t start_paddr, uint64_t size_in_byte) {
   for(uint64_t current_paddr = start_paddr; current_paddr < (start_paddr + size_in_byte); current_paddr += CACHE_LINE_SIZE_BYTE) {
     wait_for_ready();
-    *(uint64_t*)(CACHE_CTRL_BASE + CTRL_TAG_OFFSET) = get_tag(current_paddr);
-    *(uint64_t*)(CACHE_CTRL_BASE + CTRL_SET_OFFSET) = get_set(current_paddr);
-    (*(uint64_t*)CACHE_CMD_BASE) = CMD_CMO_CLEAN;
+    *(volatile uint64_t*)(CACHE_CTRL_BASE + CTRL_TAG_OFFSET) = get_tag(current_paddr);
+    *(volatile uint64_t*)(CACHE_CTRL_BASE + CTRL_SET_OFFSET) = get_set(current_paddr);
+    *(volatile uint64_t*)CACHE_CMD_BASE = CMD_CMO_CLEAN;
   }
 }
 
 void make_region_flush(uint64_t start_paddr, uint64_t size_in_byte) {
   for(uint64_t current_paddr = start_paddr; current_paddr < (start_paddr + size_in_byte); current_paddr += CACHE_LINE_SIZE_BYTE) {
     wait_for_ready();
-    *(uint64_t*)(CACHE_CTRL_BASE + CTRL_TAG_OFFSET) = get_tag(current_paddr);
-    *(uint64_t*)(CACHE_CTRL_BASE + CTRL_SET_OFFSET) = get_set(current_paddr);
-    (*(uint64_t*)CACHE_CMD_BASE) = CMD_CMO_FLUSH;
+    *(volatile uint64_t*)(CACHE_CTRL_BASE + CTRL_TAG_OFFSET) = get_tag(current_paddr);
+    *(volatile uint64_t*)(CACHE_CTRL_BASE + CTRL_SET_OFFSET) = get_set(current_paddr);
+    *(volatile uint64_t*)CACHE_CMD_BASE = CMD_CMO_FLUSH;
   }
 }
