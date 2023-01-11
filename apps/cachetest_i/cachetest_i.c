5b64ae3768bdf3487212da297459089ecc4d38cc
haohao <275626310@qq.com>
2023-01-11 11:37:04 +0800

    test mem latency

--- apps/cachetest_i/cachetest_i.c
+++ apps/cachetest_i/cachetest_i.c
@@ -2,13 +2,14 @@
 #include "cachetest_i.h"
 
 int main(){
-    unsigned long long busy_cycles_l1,busy_cycles_l2,busy_cycles_l3;
-    unsigned long long busy_instrs_l1,busy_instrs_l2,busy_instrs_l3;
+    unsigned long long busy_cycles_l1,busy_cycles_l2,busy_cycles_l3,busy_cycles_mem;
+    unsigned long long busy_instrs_l1,busy_instrs_l2,busy_instrs_l3,busy_instrs_mem;
     uint64_t start_addr = _PERF_TEST_ADDR_BASE;
 
     uint64_t end_addr_l2 = (_PERF_TEST_ADDR_BASE + _TEST_NUM_SIZE + (_PERF_L2_SIZE_BYTE/2));
     uint64_t end_addr_l1 = (_PERF_TEST_ADDR_BASE + _PERF_L1_SIZE_BYTE - (2*_TEST_NUM_SIZE));
     uint64_t end_addr_l3 = (_PERF_TEST_ADDR_BASE + _PERF_L1_SIZE_BYTE + _PERF_L2_SIZE_BYTE + _PERF_L2_SIZE_BYTE);
+    uint64_t end_addr_mem = (_PERF_TEST_ADDR_BASE + _PERF_L1_SIZE_BYTE + _PERF_L2_SIZE_BYTE + _PERF_L3_SIZE_BYTE + _PERF_L2_SIZE_BYTE);
 
     full_cache_init_i(start_addr,end_addr_l1,_PERF_CACHELINE_SIZE_BYTE,1);
     cache_iloop(&busy_instrs_l1,&busy_cycles_l1);
@@ -19,6 +20,8 @@ int main(){
     full_cache_init_i(start_addr,end_addr_l3,_PERF_CACHELINE_SIZE_BYTE,3);
     cache_iloop(&busy_instrs_l3,&busy_cycles_l3);
 
+    full_cache_init_i(start_addr,end_addr_mem,_PERF_CACHELINE_SIZE_BYTE,3);
+    cache_iloop(&busy_instrs_mem,&busy_cycles_mem);
 
     printf("the test of load-add-load hit in l1\n");
     printf("instrs %d cycles %d\n",busy_instrs_l1,busy_cycles_l1);
@@ -26,8 +29,8 @@ int main(){
     printf("instrs %d cycles %d\n",busy_instrs_l2,busy_cycles_l2);
     printf("the test of load-add-load hit in l3\n");
     printf("instrs %d cycles %d\n",busy_instrs_l3,busy_cycles_l3);
-    //printf("the test of load-add-load hit in mem\n");
-    //printf("instrs %d cycles %d\n",busy_instrs_mem,busy_cycles_mem);
+    printf("the test of load-add-load hit in mem\n");
+    printf("instrs %d cycles %d\n",busy_instrs_mem,busy_cycles_mem);
     printf("the test is finish\n");
 
 
