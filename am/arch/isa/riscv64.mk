91e9c7c0085e1fb5b28be0038174f704b31b5397
Xu, Zefan <ceba_robot@outlook.com>
2024-09-02 19:08:30 +0800

    build: add variables for march and optimization level

--- am/arch/isa/riscv64.mk
+++ am/arch/isa/riscv64.mk
@@ -1,10 +1,12 @@
+MARCH ?= rv64gc
+
 ifeq ($(LINUX_GNU_TOOLCHAIN),1)
 CROSS_COMPILE := riscv64-linux-gnu-
-COMMON_FLAGS  := -fno-pic -march=rv64gc -mcmodel=medany
 else
 CROSS_COMPILE := riscv64-unknown-linux-gnu-
-COMMON_FLAGS  := -fno-pic -march=rv64gc_zba_zbb_zbc_zbs_zbkb_zbkc_zbkx_zknd_zkne_zknh_zkr_zksed_zksh_zkt -mcmodel=medany
 endif
+
+COMMON_FLAGS  := -fno-pic -march=$(MARCH) -mcmodel=medany
 CFLAGS        += $(COMMON_FLAGS) -static
 ASFLAGS       += $(COMMON_FLAGS) -O0
 LDFLAGS       += -melf64lriscv
--- am/arch/riscv64-xs-flash.mk
+++ am/arch/riscv64-xs-flash.mk
@@ -1,3 +1,5 @@
+MARCH ?= rv64gc_zba_zbb_zbc_zbs_zbkb_zbkc_zbkx_zknd_zkne_zknh_zkr_zksed_zksh_zkt
+
 include $(AM_HOME)/am/arch/isa/riscv64.mk
 
 AM_SRCS := noop/isa/riscv/trm_flash.c \
