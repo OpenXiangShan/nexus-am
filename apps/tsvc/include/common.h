9a27ea26bf8739734d41e80b33eccef0bb567b7a
tqxu23 <tqxu23@sjtu.edu.cn>
2025-04-08 17:45:35 +0800

    add benchmark tsvc

---
+++ apps/tsvc/include/common.h
@@ -0,0 +1,35 @@
+#ifndef TSVC_COMMON_HDR
+#define TSVC_COMMON_HDR
+
+// #define iterations 100000
+#define iterations 100
+#define LEN_1D 32000
+#define LEN_2D 256
+
+// #include <sys/time.h>
+#include "undefined_lib.h"
+#include <klib.h>
+#include <stdint.h>
+
+struct args_t {
+    struct timeval t1;
+    struct timeval t2;
+    void * __restrict__ arg_info;
+};
+
+#if 0
+typedef double real_t;
+#define ABS fabs
+#else
+typedef float real_t;
+#define ABS fabsf
+#endif
+
+int dummy(real_t[LEN_1D], real_t[LEN_1D], real_t[LEN_1D], real_t[LEN_1D], real_t[LEN_1D], real_t[LEN_2D][LEN_2D], real_t[LEN_2D][LEN_2D], real_t[LEN_2D][LEN_2D], real_t);
+
+void init(int** ip, real_t* s1, real_t* s2);
+
+int initialise_arrays(const char* name);
+real_t calc_checksum(const char * name);
+
+#endif
